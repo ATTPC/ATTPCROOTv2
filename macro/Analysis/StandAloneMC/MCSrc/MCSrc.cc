04d0468fed8ea08acedcfccbfeeeae441987207d
Yassid <ayyadlim@nscl.msu.edu>
2016-02-07 20:33:39 -0500

    Stand alone program finalized

--- macro/Analysis/StandAloneMC/MCSrc/MCSrc.cc
+++ macro/Analysis/StandAloneMC/MCSrc/MCSrc.cc
@@ -1,16 +1,64 @@
 #include "MCSrc.hh"
+#include <ios>
 #include <iostream>
-#include "TH1.h"
-#include "ATPad.hh"
+#include <istream>
+#include <limits>
+
+#include "TClonesArray.h"
+#include "TString.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TTreeReader.h"
+#include "TTreePlayer.h"
+#include "TTreeReaderValue.h"
+#include "TSystem.h"
+#include "TH1F.h"
+#include "TCanvas.h"
+
+#include "ATEvent.hh"
+#include "ATHoughSpace.hh"
+#include "ATHoughSpaceLine.hh"
+
+#include "FairRootManager.h"
+#include "FairLogger.h"
+#include "FairRun.h"
+#include "FairRunAna.h"
 
 Int_t main()
 {
 
-  #pragma omp parallel for ordered schedule(dynamic,1)
-  for(Int_t i=0;i<100;i++)std::cout<<" KUKU "<<std::endl;
+    gSystem->Load("libCling.so");
+    gSystem->Load("libATTPCReco.so");
 
-  ATPad pad;
+    FairRunAna* run = new FairRunAna(); //Forcing a dummy run
 
-  return 0;
+    TString workdir = getenv("VMCWORKDIR");
+    TString FileNameHead = "output";
+    TString FilePath = workdir + "/macro/Unpack_GETDecoder2/";
+    TString FileNameTail = ".root";
+    TString FileName     = FilePath + FileNameHead + FileNameTail;
+
+    std::cout<<" Opening File : "<<FileName.Data()<<std::endl;
+    TFile* file = new TFile(FileName.Data(),"READ");
+
+    TTree* tree = (TTree*) file -> Get("cbmsim");
+    Int_t nEvents = tree -> GetEntries();
+    std::cout<<" Number of events : "<<nEvents<<std::endl;
+
+    TTreeReader Reader1("cbmsim", file);
+    TTreeReaderValue<TClonesArray> eventArray(Reader1, "ATEventH");
+    TTreeReaderValue<TClonesArray> houghArray(Reader1, "ATHough");
+
+          while (Reader1.Next()) {
+
+              ATEvent* event = (ATEvent*) eventArray->At(0);
+              ATHoughSpaceLine* fHoughSpaceLine_buff  = dynamic_cast<ATHoughSpaceLine*> (houghArray->At(0));
+
+
+          }
+
+  //#pragma omp parallel for ordered schedule(dynamic,1)
+  //for(Int_t i=0;i<100;i++)std::cout<<" Hello ATTPCer! "<<std::endl;
+   return 0;
 
 }
