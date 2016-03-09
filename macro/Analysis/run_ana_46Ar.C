19b974242fb7cfe79e724fe81e66915fdeb657a4
ayyadlim <ayyadlim@nscl.msu.edu>
2016-03-09 18:00:49 -0500

    Several kinematic cuts for 46Ar, root file create to store the results in a very simple way

--- macro/Analysis/run_ana_46Ar.C
+++ macro/Analysis/run_ana_46Ar.C
@@ -32,7 +32,7 @@ void myflush ( std::istream& in );
 void mypause();
 
 void run_ana_46Ar(TString FileNameHead = "run_",
-Int_t num_ev=100000000, Int_t file_ini=87, Int_t file_end=89, Int_t runnum=250, TString file="../Kinematics/Decay_kinematics/Kine.txt")
+Int_t num_ev=100000000, Int_t file_ini=86, Int_t file_end=93, Int_t runnum=250, TString file="../Kinematics/Decay_kinematics/Kine.txt")
 {
 
 	    //gStyle->SetCanvasPreferGL(1);
@@ -49,7 +49,12 @@ Int_t num_ev=100000000, Int_t file_ini=87, Int_t file_end=89, Int_t runnum=250, 
 			TCanvas *c4 = new TCanvas("c4","c4",200,10,700,700);
 	    c4->Divide(2,2);*/
 
-			TH2D* EAKine = new TH2D("EAKine","EAKine",100,0,10,500,0,180);
+			Double_t tEnerMin;
+			Double_t tNormChi2;
+			Double_t tThetaMin;
+			Double_t tVertexPos;
+
+			TH2D* EAKine = new TH2D("EAKine","EAKine",500,0,180,100,0,10);
 	    EAKine->SetMarkerColor(2);
 	    EAKine->SetMarkerStyle(20);
 	    EAKine->SetMarkerSize(0.7);
@@ -57,6 +62,12 @@ Int_t num_ev=100000000, Int_t file_ini=87, Int_t file_end=89, Int_t runnum=250, 
 			Int_t value;
 			Int_t nEve=0;
 
+			TFile *f = new TFile("analysis.root","RECREATE");
+			TTree *treeOut = new TTree("AnalysisTree","AnalysisTree");
+			treeOut->Branch("tEnerMin",&tEnerMin,"tEnerMin/D");
+			treeOut->Branch("tNormChi2",&tNormChi2,"tNormChi2/D");
+			treeOut->Branch("tThetaMin",&tThetaMin,"tThetaMin/D");
+			treeOut->Branch("tVertexPos",&tVertexPos,"tVertexPos/D");
 
 	    TChain *chain = new TChain("cbmsim");
 	    TFileCollection *filecol = new TFileCollection();
@@ -73,7 +84,12 @@ Int_t num_ev=100000000, Int_t file_ini=87, Int_t file_end=89, Int_t runnum=250, 
 				//filecol->Add(FileNameHead_chain);
 				//chain->Add(FileNameHead_chain);
 
+
 				TFile* file = new TFile(FileNameHead_chain.Data(),"READ");
+
+			if(file->IsOpen()){
+				std::cout<<" Analyzing"<<std::endl;
+
 				TTree* tree = (TTree*) file -> Get("cbmsim");
 		    Int_t nEvents = tree -> GetEntriesFast();
 		    std::cout<<" Number of events : "<<nEvents<<std::endl;
@@ -86,46 +102,104 @@ Int_t num_ev=100000000, Int_t file_ini=87, Int_t file_end=89, Int_t runnum=250, 
 						nEve++;
 
 						fHoughSpaceCircle  = dynamic_cast<ATHoughSpaceCircle*> (houghArray->At(0));
-						Double_t EnerMin   = fHoughSpaceCircle->FitParameters.sEnerMin;
-						Double_t NormChi2  = fHoughSpaceCircle->FitParameters.sNormChi2;
-						Double_t ThetaMin  = 180.0*fHoughSpaceCircle->FitParameters.sThetaMin/TMath::Pi();
-						Double_t VertexPos = fHoughSpaceCircle->FitParameters.sVertexPos.Z();
+						tEnerMin   = fHoughSpaceCircle->FitParameters.sEnerMin;
+						tNormChi2  = fHoughSpaceCircle->FitParameters.sNormChi2;
+						tThetaMin  = 180.0*fHoughSpaceCircle->FitParameters.sThetaMin/TMath::Pi();
+						tVertexPos = fHoughSpaceCircle->FitParameters.sVertexPos.Z();
 						//std::cout<<EnerMin<<"  "<<NormChi2<<std::endl;
 						if(nEve%1000==0) std::cout<<" Event : "<<nEve<<"/"<<nEvents<<std::endl;
-						if(NormChi2<4) EAKine->Fill(EnerMin,ThetaMin);
+						if(tNormChi2<4) EAKine->Fill(tThetaMin,tEnerMin);
+
+
+						treeOut->Fill();
 
 					}
 
+				}
 
+				file->Close();
 
 	}//for files
 
 
-        /*Double_t *ThetaCMS = new Double_t[20000];
-       	Double_t *ThetaLabRec = new Double_t[20000];
-				Double_t *EnerLabRec = new Double_t[20000];
-				Double_t *ThetaLabSca = new Double_t[20000];
-				Double_t *EnerLabSca = new Double_t[20000];
+	Double_t *ThetaCMS = new Double_t[20000];
+	Double_t *ThetaLabRec = new Double_t[20000];
+	Double_t *EnerLabRec = new Double_t[20000];
+	Double_t *ThetaLabSca = new Double_t[20000];
+	Double_t *EnerLabSca = new Double_t[20000];
 
-				std::ifstream *kineStr = new std::ifstream(file.Data());
-				Int_t numKin=0;
+	Double_t *ThetaCMS2 = new Double_t[20000];
+	Double_t *ThetaLabRec2 = new Double_t[20000];
+	Double_t *EnerLabRec2 = new Double_t[20000];
+	Double_t *ThetaLabSca2 = new Double_t[20000];
+	Double_t *EnerLabSca2 = new Double_t[20000];
 
-				  if(!kineStr->fail()){
-						while(!kineStr->eof()){
-								*kineStr>>ThetaCMS[numKin]>>ThetaLabRec[numKin]>>EnerLabRec[numKin]>>ThetaLabSca[numKin]>>EnerLabSca[numKin];
-								numKin++;
-						}
-					}else if(kineStr->fail()) std::cout<<" Warning : No Kinematics file found for this reaction! Please run the macro on $SIMPATH/macro/Kinematics/Decay_kinematics/Mainrel.cxx"<<std::endl;
+	Double_t *ThetaCMS3 = new Double_t[20000];
+	Double_t *ThetaLabRec3 = new Double_t[20000];
+	Double_t *EnerLabRec3 = new Double_t[20000];
+	Double_t *ThetaLabSca3 = new Double_t[20000];
+	Double_t *EnerLabSca3 = new Double_t[20000];
 
-					TGraph *Kine_AngRec_AngSca = new TGraph(numKin,ThetaLabRec,ThetaLabSca);
-					TGraph *Kine_AngRec_AngSca_vert = new TGraph(numKin,ThetaLabSca,ThetaLabRec);*/
+	TString fileKine="../Kinematics/Decay_kinematics/46Ar_p_4.1MeV.txt";
+	std::ifstream *kineStr = new std::ifstream(fileKine.Data());
+	Int_t numKin=0;
 
-				//Kine_AngRec_AngSca->Draw("C");
+		if(!kineStr->fail()){
+			while(!kineStr->eof()){
+					*kineStr>>ThetaCMS[numKin]>>ThetaLabRec[numKin]>>EnerLabRec[numKin]>>ThetaLabSca[numKin]>>EnerLabSca[numKin];
+					numKin++;
+			}
+		}else if(kineStr->fail()) std::cout<<" Warning : No Kinematics file found for this reaction! Please run the macro on $SIMPATH/macro/Kinematics/Decay_kinematics/Mainrel.cxx"<<std::endl;
+
+		TGraph *Kine_AngRec_AngSca = new TGraph(numKin,ThetaLabRec,ThetaLabSca);
+		TGraph *Kine_AngRec_AngSca_vert = new TGraph(numKin,ThetaLabSca,ThetaLabRec);
+		TGraph *Kine_AngRec_EnerRec = new TGraph(numKin,ThetaLabRec,EnerLabRec);
+
+		TString fileKine2="../Kinematics/Decay_kinematics/46Ar_p_2.64MeV.txt";
+		std::ifstream *kineStr2 = new std::ifstream(fileKine2.Data());
+	  numKin=0;
+
+		if(!kineStr2->fail()){
+			while(!kineStr2->eof()){
+					*kineStr2>>ThetaCMS2[numKin]>>ThetaLabRec2[numKin]>>EnerLabRec2[numKin]>>ThetaLabSca2[numKin]>>EnerLabSca2[numKin];
+					numKin++;
+			}
+		}else if(kineStr2->fail()) std::cout<<" Warning : No Kinematics file found for this reaction! Please run the macro on $SIMPATH/macro/Kinematics/Decay_kinematics/Mainrel.cxx"<<std::endl;
+
+		TGraph *Kine_AngRec_AngSca2 = new TGraph(numKin,ThetaLabRec2,ThetaLabSca2);
+		TGraph *Kine_AngRec_AngSca_vert2 = new TGraph(numKin,ThetaLabSca2,ThetaLabRec2);
+		TGraph *Kine_AngRec_EnerRec2 = new TGraph(numKin,ThetaLabRec2,EnerLabRec2);
+
+		/*TString fileKine3="../Kinematics/Decay_kinematics/46Ar_p_3.79MeV.txt";
+		std::ifstream *kineStr3 = new std::ifstream(fileKine3.Data());
+		numKin=0;
+
+		if(!kineStr3->fail()){
+			while(!kineStr3->eof()){
+					*kineStr3>>ThetaCMS3[numKin]>>ThetaLabRec3[numKin]>>EnerLabRec3[numKin]>>ThetaLabSca3[numKin]>>EnerLabSca3[numKin];
+					numKin++;
+			}
+		}else if(kineStr3->fail()) std::cout<<" Warning : No Kinematics file found for this reaction! Please run the macro on $SIMPATH/macro/Kinematics/Decay_kinematics/Mainrel.cxx"<<std::endl;
+
+		TGraph *Kine_AngRec_AngSca3 = new TGraph(numKin,ThetaLabRec3,ThetaLabSca3);
+		TGraph *Kine_AngRec_AngSca_vert3 = new TGraph(numKin,ThetaLabSca3,ThetaLabRec3);
+		TGraph *Kine_AngRec_EnerRec3 = new TGraph(numKin,ThetaLabRec3,EnerLabRec3);*/
+
+
+   	f->cd();
+		Kine_AngRec_EnerRec->Write();
+		Kine_AngRec_EnerRec2->Write();
+    treeOut->Write();
+		f->Close();
+
+
 				//Kine_AngRec_AngSca_vert->Draw("C");
 
 				c1->cd(1);
 				EAKine->Draw("zcol");
-
+				Kine_AngRec_EnerRec->Draw("C");
+				Kine_AngRec_EnerRec2->Draw("C");
+				//Kine_AngRec_EnerRec3->Draw("C");
 }
 
 
--- macro/Kinematics/Decay_kinematics/Mainrel.cxx
+++ macro/Kinematics/Decay_kinematics/Mainrel.cxx
@@ -30,32 +30,32 @@ int Mainrel(){
 
 
 		Int_t a=1;
-		int A1=8.; //mass0 of the incident particle
-		int Z1=2.; //charge of the incident particle
+		int A1=46.; //mass0 of the incident particle
+		int Z1=18.; //charge of the incident particle
 		char *El1=new char[2];
 
 		//double m1=77.963180;
 
 		//cout<<El1<<endl;
 
-		int A2=4.; //mass0 of the target
-		int Z2=2.; //charge of the target
+		int A2=1.; //mass0 of the target
+		int Z2=1.; //charge of the target
 		char *El2=new char[2];
 
 		//double m2=2.0141;
 
 		//cout<<El2<<endl;
 
-		int A3=8; //mass0 of the scattered particle
-		int Z3=2; //charge of the scattered particle
+		int A3=46; //mass0 of the scattered particle
+		int Z3=18; //charge of the scattered particle
 		char *El3=new char[2];
 
 		//double m3=78.971987;
 
 		//cout<<El3<<endl;
 
-		int A4=4;//mass0 of the recoil
-		int Z4=2;//charge of the recoil
+		int A4=1;//mass0 of the recoil
+		int Z4=1;//charge of the recoil
 		char *El4=new char[2];
 
 		//double m4=1.007825;
@@ -77,7 +77,7 @@ int Mainrel(){
 		double ex3=0.0; //excitation energy of the scattered particle
 		double ex4=0.0; //excitation energy of the recoil
 
-		double tbt=2.125; // incident energy (total Lab energy in MeV)
+		double tbt=4.1; // incident energy (total Lab energy in MeV)
 
 		double Energyrdec,Anglerdec;
 
