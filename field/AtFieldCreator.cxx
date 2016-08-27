8ee9167bcd07f3c07538152df84fa86a291dd978
ayyadlim <ayyadlim@localhost.localdomain>
2016-08-27 14:40:04 -0400

    Change on AtFieldCreator class

--- field/AtFieldCreator.cxx
+++ field/AtFieldCreator.cxx
@@ -62,7 +62,7 @@ FairField* AtFieldCreator::createFairField()
 	// Initialise field
 	if ( fMagneticField ) {
 		fMagneticField->Init();
-		fMagneticField->Print();
+		//fMagneticField->Print();
 	}
   }
   return fMagneticField;
