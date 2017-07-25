662f2ce20c9370a0ee3f07fa5dae0eff2cd083b2
freund17 <lukas.aymans@googlemail.com>
2017-07-25 10:42:22 -0400

    macro now works with correct root-file

--- macro/export/root_to_pcl.C
+++ macro/export/root_to_pcl.C
@@ -2,6 +2,7 @@
 #include "TFile.h"
 #include "TTree.h"
 #include "TClonesArray.h"
+#include "AtHit.hh"
 
 #include <iostream>
 #include <fstream>
@@ -15,18 +16,24 @@ void root_to_pcl()
 
 	std::cout << "Analysis of simulation file " << mcFileName << "..." << std::endl;
 
-	TFile file(mcFileName.Data());
+	TFile file(inputFile);
 	TTree *tree = nullptr;
 	file.GetObject("cbmsim", tree);
 
-	TClonesArray *pointArray = nullptr;
-	tree->SetBranchAddress("AtTpcPoint", &pointArray);
+	TClonesArray *events = nullptr;
+	TBranch *eventsBranch = tree->GetBranch("ATEventH");
+	eventsBranch->SetAddress(&events);
 
 	Int_t eventCount = tree->GetEntries();
 	for (Int_t i = 0; i < eventCount; ++i) {
 		std::cout << "# Reading event " << (i + 1) << " of " << eventCount << std::endl;
-		tree->GetEvent(i);
-		Int_t pointCount = pointArray->GetEntries();
+		eventsBranch->GetEvent(i);
+		ATEvent *event = (ATEvent*)events->At(0);
+
+		std::vector<ATHit> const *hitArray = event->GetHitArray();
+		std::cout << "HitArray Size: " << hitArray->size() << std::endl;
+
+		Int_t const hitCount = hitArray->GetEntries();
 		
 		std::ostringstream oss;
 		oss << outputFileName << "." << i << ".pcl";
@@ -36,21 +43,22 @@ void root_to_pcl()
 			<< "FIELDS x y z time intensity" << std::endl
 			<< "SIZE 4 4 4 4 4" << std::endl
 			<< "TYPE F F F U F" << std::endl
-			<< "WIDTH " << pointCount << std::endl
+			<< "WIDTH " << hitCount << std::endl
 			<< "HEIGHT 1" << std::endl
 			<< "VIEWPOINT 0 0 0 1 0 0 0" << std::endl
-			<< "POINTS " << pointCount << std::endl
+			<< "POINTS " << hitCount << std::endl
 			<< "DATA ascii" << std::endl;
 
-		for (Int_t j = 0; j < pointCount; ++j) {
-			std::cout << "# Reading point " << (j + 1) << " of " << pointCount << std::endl;
-			AtTpcPoint *point = (AtTpcPoint*)(*pointArray)[j];
+		for (Int_t j = 0; j < hitCount; ++j) {
+			std::cout << "# Reading point " << (j + 1) << " of " << hitCount << std::endl;
+			AtHit const *hit = (*hitArray)[j];
+			TVector3 position = hit->GetPosition();
 
-			outputFile << point->GetXIn() << " "
-				<< point->GetYIn() << " "
-				<< point->GetZIn() << " "
-				<< point->GetMassNum() << " " // FIXME
-				<< point->GetAIni() << std::endl; // FIXME
+			outputFile << position->X() << " "
+				<< position->Y() << " "
+				<< position->Z() << " "
+				<< hit->GetTimeStamp() << " "
+				<< hit->GetCharge() << std::endl;
 		}
 	}
 }
