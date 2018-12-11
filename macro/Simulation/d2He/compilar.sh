5587df28485d3a26bfd9ed2ec43fd255d4f678ba
Juan <zamora@nscl.msu.edu>
2018-12-11 19:38:11 -0200

    fixed missing geant4 data files

--- macro/Simulation/d2He/compilar.sh
+++ macro/Simulation/d2He/compilar.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-cd  ../../build
-#cmake ../
-make -j
+cd  ../../../build
+cmake ../
+make -j3
 source config.sh
-export G4ENSDFSTATEDATA=$ROOTSYS/share/Geant4/data/G4ENSDFSTATE1.2.1
+#export G4ENSDFSTATEDATA=$ROOTSYS/share/Geant4/data/G4ENSDFSTATE
+#export G4REALSURFACEDATA=$ROOTSYS/share/Geant4/data/RealSurface
 cd ../macro/Simulation
 
