d23ec06d662dd2fbe7212414439d8c4e1b8b1434
Yassid <ayyadlim@nscl.msu.edu>
2019-07-18 17:53:39 -0400

    Updated stand alone analysis

--- compiled/StandAloneATTPCROOT/SAMain.hh
+++ compiled/StandAloneATTPCROOT/SAMain.hh
@@ -16,3 +16,22 @@
 #include "TH1F.h"
 #include "TCanvas.h"
 #include "TStopwatch.h"
+
+#include "FairRootManager.h"
+#include "FairLogger.h"
+#include "FairRun.h"
+#include "FairRunAna.h"
+
+#include "AtTpcPoint.h"
+
+#include <pcl/console/parse.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/sample_consensus/ransac.h>
+#include <pcl/sample_consensus/sac_model_plane.h>
+#include <pcl/sample_consensus/sac_model_sphere.h>
+#include <pcl/sample_consensus/sac_model_circle.h>
+#include <boost/thread/thread.hpp>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/segmentation/sac_segmentation.h>
