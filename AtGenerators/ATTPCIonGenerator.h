b7df1e5d1faef605d237caf23f3b26b59b294784
Yassid <ayyadlim@nscl.msu.edu>
2020-08-28 20:02:35 -0400

    Reverting back the Ion Generator

--- AtGenerators/ATTPCIonGenerator.h
+++ AtGenerators/ATTPCIonGenerator.h
@@ -26,8 +26,8 @@ class ATTPCIonGenerator : public FairGenerator
   ATTPCIonGenerator();
 
 
-  /** Constructor with ion name
-    ** For the generation of ions with pre-defined FairIon
+  /** Constructor with ion name    
+    ** For the generation of ions with pre-defined FairIon 
    ** By default, the  excitation energy is zero. This can be changed with the
    ** respective modifiers.
    **@param ionName  Ion name
@@ -35,13 +35,13 @@ class ATTPCIonGenerator : public FairGenerator
    **@param px,py,pz  Momentum components [GeV] per nucleon!
    **@param vx,vy,vz  Vertex coordinates [cm]
    **/
-  ATTPCIonGenerator(const Char_t* ionName, Int_t mult, Double_t px,
+  ATTPCIonGenerator(const Char_t* ionName, Int_t mult, Double_t px, 
 		  Double_t py, Double_t pz);
 
 
-  /** Default constructor
+  /** Default constructor 
    ** For the generation of ions with atomic number z and mass number a.
-   ** By default, the mass equals a times the proton mass and the
+   ** By default, the mass equals a times the proton mass and the 
    ** excitation energy is zero. This can be changed with the
    ** respective modifiers.
    **@param z         Atomic number
@@ -50,10 +50,12 @@ class ATTPCIonGenerator : public FairGenerator
    **@param mult      Number of ions per event
    **@param px,py,pz  Momentum components [GeV] per nucleon!
    **@param vx,vy,vz  Vertex coordinates [cm]
+   **@param ener      Energy of the ion.
+   **@param eLoss     Maximum energy loss before reaction happens. Defaults to ener.
    **/
-  ATTPCIonGenerator(const char* name,Int_t z, Int_t a, Int_t q, Int_t mult, Double_t px,
-		  Double_t py, Double_t pz, Double_t Ex, Double_t m, Double_t ener, Double_t ebeam, 
-		  Double_t beam1, Double_t beam2, Double_t beam3);
+  ATTPCIonGenerator(const char* name,Int_t z, Int_t a, Int_t q, Int_t mult, Double_t px, 
+		    Double_t py, Double_t pz, Double_t Ex, Double_t m, Double_t ener,
+		    Double_t eLoss = -1);
 
 
   ATTPCIonGenerator(const ATTPCIonGenerator&);
@@ -71,13 +73,13 @@ class ATTPCIonGenerator : public FairGenerator
   void SetExcitationEnergy(Double_t eExc);
   void SetMass(Double_t mass);
 
-  void SetSpotRadius   (Double32_t r=0, Double32_t focptz=0, Double32_t off=0) {
-    fR=r; fOcal=focptz; fOcalUncert=off; fBeamSpotIsSet=kTRUE;}
+  void SetSpotRadius   (Double32_t r=0, Double32_t z=0, Double32_t off=0) {
+    fR=r; fz=z; fOffset=off; fBeamSpotIsSet=kTRUE;}
 
 
-  /** Method ReadEvent
+  /** Method ReadEvent 
    ** Generates <mult> of the specified ions and hands hem to the
-   ** FairPrimaryGenerator.
+   ** FairPrimaryGenerator. 
    **/
   virtual Bool_t ReadEvent(FairPrimaryGenerator* primGen);
 
@@ -87,24 +89,20 @@ private:
   static Int_t fgNIon;      //! Number of the instance of this class
   Int_t    fMult;           // Multiplicity per event
   Double_t fPx, fPy, fPz;   // Momentum components [GeV] per nucleon
-  Double32_t fR, fOcal, fOcalUncert;  // beam Spot radius [cm], z source, z source uncertainty
-  Double_t fz;
+  Double32_t fR, fz, fOffset;  // beam Spot radius [cm], z source, y source
   Double_t fVx, fVy, fVz;   // Vertex coordinates [cm]
-  Double_t fEbeam; // Beam energy [MeV] per nucleon
   FairIon   *fIon;          // Pointer to the FairIon to be generated
   Int_t    fQ;		    // Electric charge [e]
   Int_t fNomEner;
-  Int_t fA;
-  Double_t fwhmFocus;
-  Double_t angularDiv;
-  Double_t zFocus;
-
+  Double_t fMaxEnLoss;      // Max energy loss before reation happens
 
   Bool_t fBeamSpotIsSet;    // True if point beamspot is set
-
+    
   ClassDef(ATTPCIonGenerator,1)
 
-};
+}; 
 
 
 #endif
+   
+
