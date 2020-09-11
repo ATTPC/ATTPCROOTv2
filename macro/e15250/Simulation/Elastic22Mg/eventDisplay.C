2c45f9aedb7835f92e68f180d4ff45ab18cca8a7
DanielRegueira <daniel.regueira@rai.usc.es>
2020-09-11 16:34:31 +0200

    for the simulation

---
+++ macro/e15250/Simulation/Elastic22Mg/eventDisplay.C
@@ -0,0 +1,31 @@
+void eventDisplay()
+{
+  //-----User Settings:-----------------------------------------------
+  TString  InputFile     ="./data/attpcsim_e15250.root";
+  TString  ParFile       ="./data/attpcpar_e15250.root";
+  TString  OutFile	     ="./data/attpctest_e15250.root";
+
+
+  // -----   Reconstruction run   -------------------------------------------
+  FairRunAna *fRun= new FairRunAna();
+  fRun->SetInputFile(InputFile.Data());
+  fRun->SetOutputFile(OutFile.Data());
+
+  FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
+  FairParRootFileIo* parInput1 = new FairParRootFileIo();
+  parInput1->open(ParFile.Data());
+  rtdb->setFirstInput(parInput1);
+
+  FairEventManager *fMan= new FairEventManager();
+
+  //----------------------Traks and points -------------------------------------
+  FairMCTracks    *Track     = new FairMCTracks("Monte-Carlo Tracks");
+  FairMCPointDraw *AtTpcPoints = new FairMCPointDraw("AtTpcPoint", kBlue, kFullSquare);
+
+  fMan->AddTask(Track);
+  fMan->AddTask(AtTpcPoints);
+
+
+  fMan->Init();
+
+}
