cd74e8e5931428d80e11bf077202421fad3bebec
Bruno Olaizola Mampaso <bolaizol@brunopc.dyndns.cern.ch>
2021-05-05 09:56:17 +0200

    SpecMAT padplane geometry

---
+++ AtMap/AtSpecMATMap.h
@@ -0,0 +1,34 @@
+/*********************************************************************
+ *   SpecMAT Mapping Class				             *
+ *   Author: B. Olaizola		        		     *
+ *   Log: 2/26/2021					             *
+ *								     *
+ *********************************************************************/
+
+#ifndef ATSPECMATMAP_H
+#define ATSPECMATMAP_H
+
+#include <boost/multi_array.hpp>
+
+#include "AtMap.h"
+
+class AtSpecMATMap : public AtMap {
+
+public:
+   AtSpecMATMap();
+   ~AtSpecMATMap();
+
+   void Dump() override;                                         // pure virtual member
+   void GenerateAtTpc() override;                                // pure virtual member
+   std::vector<Float_t> CalcPadCenter(Int_t PadRef) override;    // pure virtual member
+   Int_t BinToPad(Int_t binval) override { return binval - 1; }; // pure virtual member
+
+   TH2Poly *GetAtTpcPlane() override; // virtual member
+
+   void SpecMATPadPlane();
+
+
+   ClassDefOverride(AtSpecMATMap, 1);
+};
+
+#endif
