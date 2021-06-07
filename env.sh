0389d09e90f48ab145451ba6e0c87ef722d91085
Adam Anthony <anthonya@nscl.msu.edu>
2021-06-07 12:51:44 -0400

    Build against GENFIT on fishtank

--- env.sh
+++ env.sh
@@ -3,3 +3,4 @@
 export SIMPATH=/mnt/misc/sw/x86_64/Debian/10/fairroot/18.6.3/fairsoft
 export FAIRROOTPATH=/mnt/misc/sw/x86_64/Debian/10/fairroot/18.6.3/fairroot
 export CMAKE_PREFIX_PATH=/mnt/misc/sw/x86_64/Debian/10/fairroot/18.6.3:${CMAKE_PREFIX_PATH}
+export GENFIT=/mnt/misc/sw/x86_64/Debian/10/fairroot/18.6.3/genfit
