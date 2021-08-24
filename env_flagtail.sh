046f172c83da8e15916d7a35448a351c423d2720
Adam Anthony <anthonya@nscl.msu.edu>
2021-08-24 17:54:53 -0400

    Update build scripts
    Generalized FindGENFIT2 macro so it does not depend on the enviroment
    variable $GENFIT being properly set. Instead use cmake tools to find
    the library.
    
    Update enviroment script to respect any change to enviroment variables
    made before running (will not override PATH etc, but prepend).

--- env_flagtail.sh
+++ env_flagtail.sh
@@ -2,9 +2,9 @@
 module purge
 module load fairroot/18.6.3
 #module load xerces/3.2.3
-source build/config.sh -p
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-
+source $SCRIPT_DIR/build/config.sh -p
 
 #export PATH=/mnt/analysis/e12014/clangInstall/install/bin/:$PATH
-export LD_LIBRARY_PATH=/mnt/analysis/e12014/clangInstall/install/lib:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=/mnt/analysis/e12014/clangInstall/install/lib:$LD_LIBRARY_PATH
