0697a0f717cfebb938e64998bbf8ca179c3cf5d1
ACeulemans <69502397+ACeulemans@users.noreply.github.com>
2022-03-04 11:45:13 +0100

    Add macro files for unpacking
    run_unpack_SpecMAT is for decoding + PSA
    run_eve is for visualizing the results

---
+++ macro/Unpack_ROOT/run_unpack_SpecMAT.C
@@ -0,0 +1,181 @@
+#define cRED "\033[1;31m"
+#define cYELLOW "\033[1;33m"
+#define cNORMAL "\033[0m"
+#define cGREEN "\033[1;32m"
+
+#include <sys/stat.h>
+
+bool check_file(const std::string &name);
+
+void run_unpack_SpecMAT(TString dataFile = "./data/TTreesGETrun_9993.root",
+                        TString mappath = "../../") {
+
+  if (!check_file(dataFile.Data())) {
+    std::cout << cRED << " Run file " << dataFile.Data()
+              << " not found! Terminating..." << cNORMAL << std::endl;
+    exit(0);
+  }
+
+  // -----   Timer   --------------------------------------------------------
+  TStopwatch timer;
+  timer.Start();
+  // ------------------------------------------------------------------------
+
+  gSystem->Load("libXMLParser.so");
+  // -----------------------------------------------------------------
+  // Set file names
+  TString scriptfile = "LookupSpecMATnoScint.xml";
+  TString dir = getenv("VMCWORKDIR");
+  TString scriptdir = dir + "/scripts/" + scriptfile;
+  TString dataDir = dir + "/macro/data/";
+  TString geomDir = dir + "/geometry/";
+  gSystem->Setenv("GEOMPATH", geomDir.Data());
+
+  // Parameter file name
+  TString parFile = "./data/attpcpar.root";
+
+  TString outputFile = "./data/run_9993.root";
+  // TString mcParFile   = dataDir + name + ".params.root";
+  TString loggerFile = dataDir + "SpecMATLog.log";
+
+  TString parameterFile = "SpecMAT.run_9993.par";
+  TString triggerFile = "SpecMAT.trigger.par";
+  TString trigParFile = dir + "/parameters/" + triggerFile;
+  TString digiParFile = dir + "/parameters/" + parameterFile;
+
+  TString geoManFile = geomDir + "SpecMAT_He1Bar.root";
+
+  TString inimap = mappath + "inhib.txt";
+  TString lowgmap = mappath + "lowgain.txt";
+  TString xtalkmap = mappath + "beampads_e15503b.txt";
+
+  // -----------------------------------------------------------------
+  // Logger
+  FairLogger *fLogger = FairLogger::GetLogger();
+  fLogger->SetLogFileName(loggerFile.Data());
+  fLogger->SetLogToScreen(kTRUE);
+  fLogger->SetLogToFile(kTRUE);
+  fLogger->SetLogVerbosityLevel("MEDIUM");
+
+  FairRunAna *run = new FairRunAna();
+  run->SetOutputFile(outputFile);
+  run->SetGeomFile(geoManFile);
+
+  FairRuntimeDb *rtdb = run->GetRuntimeDb();
+  FairParAsciiFileIo *parIo1 = new FairParAsciiFileIo();
+  parIo1->open(digiParFile.Data(), "in");
+  rtdb->setFirstInput(parIo1);
+  FairParAsciiFileIo *parIo2 = new FairParAsciiFileIo();
+  parIo2->open(trigParFile.Data(), "in");
+  rtdb->setSecondInput(parIo2);
+
+  // ------------------------------------------------------------------------
+
+  // Settings
+  Bool_t fUseDecoder = kTRUE;
+  if (dataFile.IsNull() == kTRUE)
+    fUseDecoder = kFALSE;
+
+  Bool_t fUseSeparatedData = kFALSE;
+  if (dataFile.EndsWith(".txt"))
+    fUseSeparatedData = kTRUE;
+
+  /*
+   *     Unpacking options:
+   *         - SetUseSeparatedData:      To be used with 10 CoBo files without
+   * merging. Mainly for the ATTPC. Enabled if the input file is a txt.
+   *         - SetPseudoTopologyFrame:   Used to force the graw file to have a
+   * Topology frame.
+   *         - SetPersistance:           Save the unpacked data into the root
+   * file.
+   *         - SetMap:                   Chose the lookup table.
+   *         - SetMapOpt                 Chose the pad plane geometry. In
+   * addition forces the unpacker to use Basic Frames for 1 single file (p-ATTPC
+   * case) of Layered Frames for Merged Data (10 Cobos merged data).
+   */
+  AtDecoderSpecMATTask *fDecoderTask = new AtDecoderSpecMATTask();
+  fDecoderTask->SetUseSeparatedData(fUseSeparatedData);
+  if (fUseSeparatedData)
+    fDecoderTask->SetPseudoTopologyFrame(
+        kTRUE); //! This calls the method 10 times so for less than 10 CoBos
+                //! ATCore2 must be modified
+  Bool_t IsCoboPositivePolarity[4] = {
+      true, false, true,
+      true}; // Positive polarity for padplane, negative for scintillators
+  fDecoderTask->SetPositivePolarity(IsCoboPositivePolarity);
+  fDecoderTask->SetPersistence(kTRUE);
+  fDecoderTask->SetMap(scriptdir.Data());
+  fDecoderTask->SetNumCobo(4);
+  Bool_t IsCoboPadPlane[4] = {true, false, true, true};
+  fDecoderTask->SetIsCoboPadPlane(IsCoboPadPlane);
+  // fDecoderTask -> SetInhibitMaps(inimap,lowgmap,xtalkmap); // TODO: Only
+  // implemented for fUseSeparatedData!!!!!!!!!!!!!!!!!!!1
+  fDecoderTask->SetMapOpt(2); // Does not do anything for SpecMAT for the moment
+  fDecoderTask->AddData(dataFile);
+
+  run->AddTask(fDecoderTask);
+
+  AtPulseTask *pulse = new AtPulseTask();
+  pulse->SetPersistence(kTRUE);
+  pulse->SetSaveMCInfo();
+  pulse->SelectDetectorId(kSpecMAT);
+  run->AddTask(pulse);
+
+  AtPSASimple2 *psa = new AtPSASimple2();
+  AtPSAtask *psaTask = new AtPSAtask(psa);
+  psaTask->SetPersistence(kTRUE);
+  psa->SetThreshold(50);
+  // psaTask -> SetPSAMode(1); //NB: 1 is ATTPC - 2 is pATTPC
+  // psaTask -> SetPeakFinder(); //NB: Use either peak finder of maximum finder
+  // but not both at the same time
+  psa->SetMaxFinder();
+  // psa-> SetBaseCorrection(kTRUE); //Directly apply the base line correction
+  // to the pulse amplitude to correct for the mesh induction. If false the
+  // correction is just saved psa -> SetTimeCorrection(kTRUE); //Interpolation
+  // around the maximum of the signal peak. Only affect Z calibration at PSA
+  // stage
+  run->AddTask(psaTask);
+
+  /*
+   AtHoughTask *HoughTask = new AtHoughTask();
+   HoughTask ->SetPersistence(kTRUE);
+   HoughTask ->SetLinearHough();
+   //HoughTask ->SetCircularHough();
+   HoughTask ->SetHoughThreshold(50.0); // Charge threshold for Hough
+   HoughTask ->SetEnableMap(); //Enables an instance of the ATTPC map:  This
+   enables the MC with Q instead of position HoughTask
+   ->SetMap(scriptdir.Data()); run -> AddTask(HoughTask);
+
+   AtPRAtask *praTask = new AtPRAtask();
+    praTask->SetPersistence(kTRUE);
+    run->AddTask(praTask);
+
+    AtClusterizeTask *clusterizer = new AtClusterizeTask();
+    clusterizer->SetPersistence(kFALSE);
+    run->AddTask(clusterizer);
+    */
+
+  run->Init();
+
+  // run -> RunOnTBData();
+  run->Run(0, 30);
+
+  std::cout << std::endl << std::endl;
+  std::cout << "Macro finished succesfully." << std::endl << std::endl;
+  std::cout << "- Output file : " << outputFile << std::endl << std::endl;
+  // -----   Finish   -------------------------------------------------------
+  timer.Stop();
+  Double_t rtime = timer.RealTime();
+  Double_t ctime = timer.CpuTime();
+  cout << endl << endl;
+  cout << "Real time " << rtime << " s, CPU time " << ctime << " s" << endl;
+  cout << endl;
+  // ------------------------------------------------------------------------
+
+  gApplication->Terminate();
+}
+
+bool check_file(const std::string &name) {
+  struct stat buffer;
+  return (stat(name.c_str(), &buffer) == 0);
+}
