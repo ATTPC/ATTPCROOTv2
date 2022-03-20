8b0fcd6f500288c2a7dddbfa439ede216ee07c26
Adam Anthony <anthonya@nscl.msu.edu>
2022-03-20 15:32:08 -0400

    Move unpacking tasks to new library

---
+++ AtUnpack/AtUnpackLinkDef.h
@@ -0,0 +1,28 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class GETDecoder2 + ;
+#pragma link C++ class GETFrameInfo + ;
+#pragma link C++ class GETHeaderBase + ;
+#pragma link C++ class GETBasicFrameHeader + ;
+#pragma link C++ class GETLayerHeader + ;
+#pragma link C++ class GETTopologyFrame + ;
+#pragma link C++ class GETBasicFrame + ;
+#pragma link C++ class GETCoboFrame + ;
+#pragma link C++ class GETLayeredFrame + ;
+#pragma link C++ class GETFileChecker + ;
+#pragma link C++ class GETMath2 + ;
+
+#pragma link C++ class AtCore2 + ;
+#pragma link C++ class AtPedestal + ;
+#pragma link C++ class AtCoreSpecMAT + ;
+#pragma link C++ class AtHDFParser + ;
+
+#pragma link C++ class AtDecoder2Task + ;
+#pragma link C++ class AtDecoderSpecMATTask + ;
+#pragma link C++ class AtHDFParserTask + ;
+
+#endif
