15b38e0d9d30611eae76e41e33d5100737174fd8
Adam Anthony <anthonya@nscl.msu.edu>
2022-03-25 15:39:52 -0400

    Update FindROOT to first seach for config
    If it cannot it will default to old behavior

--- cmake/modules/FindROOT.cmake
+++ cmake/modules/FindROOT.cmake
@@ -7,15 +7,16 @@
  ################################################################################
 # - Find ROOT instalation
 # This module tries to find the ROOT installation on your system.
-# It tries to find the root-config script which gives you all the needed 
-# information.
+# It will start by looking for a ROOTConfig.cmake file and use that to import ROOT
+#
+# If it cannot find ROOT that way, it will fall back to trying to use root-config
+# script which gives you all the needed information.
 # If the system variable ROOTSYS is set this is straight forward.
 # If not the module uses the pathes given in ROOT_CONFIG_SEARCHPATH.
 # If you need an other path you should add this path to this varaible.  
 # The root-config script is then used to detect basically everything else.
-# This module defines a number of key variables and macros.
-#
-# Variables defined by this module:
+# This module defines a number of key variables, macros, and targets.
+# Variables defined by this module using root-config version:
 #
 #   ROOT_FOUND               System has ROOT, this means the root-config 
 #                            executable was found.
@@ -41,69 +42,107 @@
 #   ROOT_CINT_EXECUTABLE     The rootcint executable.
 #
 #   RLIBMAP_EXECUTABLE       The rlibmap executable.
-
-Message(STATUS "Looking for Root...")
-
-Set(ROOT_CONFIG_SEARCHPATH
-  ${SIMPATH}/bin
-  ${SIMPATH}/tools/root/bin
-  $ENV{ROOTSYS}/bin
-)
-
-Set(ROOT_FOUND FALSE)
-Set(ROOT_DEFINITIONS "")
-Set(ROOT_INSTALLED_VERSION_TOO_OLD FALSE)
-Set(ROOT_CONFIG_EXECUTABLE ROOT_CONFIG_EXECUTABLE-NOTFOUND)
-
-Find_Program(ROOT_CONFIG_EXECUTABLE NAMES root-config 
-             PATHS ${ROOT_CONFIG_SEARCHPATH}
-             NO_DEFAULT_PATH
-            )
-     
-If(ROOT_CONFIG_EXECUTABLE)
-   
-  String(REGEX REPLACE "(^.*)/bin/root-config" "\\1" test ${ROOT_CONFIG_EXECUTABLE}) 
-  Set(ENV{ROOTSYS} ${test})
-  Set(ROOTSYS ${test})
-
-  Execute_Process(COMMAND ${ROOT_CONFIG_EXECUTABLE} --version 
-                  OUTPUT_VARIABLE ROOT_VERSION_STRING
-                 )
-  Execute_Process(COMMAND ${ROOT_CONFIG_EXECUTABLE} --prefix
-                  OUTPUT_VARIABLE ROOT_INSTALL_DIR
-                 )
-  String(STRIP ${ROOT_VERSION_STRING} ROOT_VERSION_STRING)
-  String(STRIP ${ROOT_INSTALL_DIR} ROOT_INSTALL_DIR)
+#
+# Targets manually defined by this module if using root-config version:
+# VMC Core RIO Tree Rint Physics MathCore Thread Geom EG Eve Spectrum TreePlayer GenVector
+#
+# You can change the behavior of this script by using the following variables:
+#
+#   ROOT_ROOT
+#     Specific path to the ROOTConfig.cmake file    
+#
+#   ROOT_NO_FIND_PACKAGE_CONFIG_FILE
+#     Skip looking for ROOTConfig.cmake file
 
 
-  MESSAGE(STATUS "Looking for Root... - Found ${ROOT_INSTALL_DIR}/bin/root")
-  MESSAGE(STATUS "Looking for Root... - Found version is ${ROOT_VERSION_STRING} ")   
-   
-  # extract major, minor, and patch versions from
-  # the version string given by root-config
-  String(REGEX REPLACE "^([0-9]+)\\.[0-9][0-9]+\\/[0-9][0-9]+.*" "\\1" ROOT_VERSION_MAJOR "${ROOT_VERSION_STRING}")
-  String(REGEX REPLACE "^[0-9]+\\.([0-9][0-9])+\\/[0-9][0-9]+.*" "\\1" ROOT_VERSION_MINOR "${ROOT_VERSION_STRING}")
-  String(REGEX REPLACE "^[0-9]+\\.[0-9][0-9]+\\/([0-9][0-9]+).*" "\\1" ROOT_VERSION_PATCH "${ROOT_VERSION_STRING}")
 
-  # compute overall version numbers which can be compared at once
-  Math(EXPR req_vers "${ROOT_FIND_VERSION_MAJOR}*10000 + ${ROOT_FIND_VERSION_MINOR}*100 + ${ROOT_FIND_VERSION_PATCH}")
-  Math(EXPR found_vers "${ROOT_VERSION_MAJOR}*10000 + ${ROOT_VERSION_MINOR}*100 + ${ROOT_VERSION_PATCH}")
+if(NOT ROOT_ROOT)
+  set(ROOT_ROOT $ENV{ROOT_ROOT})
+endif()
+if(ROOT_ROOT)
+  set(_ROOT_SEARCH_OPTS NO_DEFAULT_PATH)
+else()
+  set(_ROOT_SEARCH_OPTS)
+endif()
 
-  Set(ROOT_Version ${found_vers})
-  Set(ROOT_VERSION_NUMBER ${found_vers})
+if(NOT ROOT_FOUND AND NOT ROOT_NO_FIND_PACKAGE_CONFIG_FILE)
+  message(STATUS "Looking for ROOT using config")
+  # Require at least 6.16 to be considered valid
+  # previous versions were not configured correctly
+  find_package(ROOT QUIET CONFIG 6.16
+    HINTS ${ROOT_ROOT}
+    ${_ROOT_SEARCH_OPTS}
+    )
 
-  If(found_vers LESS req_vers)
-    Set(ROOT_FOUND FALSE)
-    Set(ROOT_INSTALLED_VERSION_TOO_OLD TRUE)
-  Else(found_vers LESS req_vers)
-    Set(ROOT_FOUND TRUE)
-  EndIf(found_vers LESS req_vers)
+  if(ROOT_FOUND)
+    message(STATUS "Found ROOT version ${ROOT_VERSION}")
+    return()
+  endif()
+endif()
 
-Else(ROOT_CONFIG_EXECUTABLE)
-  Message(STATUS "Looking for Root... - Not found")
-  Message(FATAL_ERROR "ROOT not installed in the searchpath and ROOTSYS is not set. Please set ROOTSYS or add the path to your ROOT installation in the Macro FindROOT.cmake in the subdirectory cmake/modules.")
-Endif(ROOT_CONFIG_EXECUTABLE)
+if(NOT ROOT_FOUND)
+  message(STATUS "Looking for ROOT using root-config")
 
+  Set(ROOT_CONFIG_SEARCHPATH
+    ${SIMPATH}/bin
+    ${SIMPATH}/tools/root/bin
+    $ENV{ROOTSYS}/bin
+    )
+
+  Set(ROOT_FOUND FALSE)
+  Set(ROOT_DEFINITIONS "")
+  Set(ROOT_INSTALLED_VERSION_TOO_OLD FALSE)
+  Set(ROOT_CONFIG_EXECUTABLE ROOT_CONFIG_EXECUTABLE-NOTFOUND)
+
+  Find_Program(ROOT_CONFIG_EXECUTABLE NAMES root-config 
+    PATHS ${ROOT_CONFIG_SEARCHPATH}
+    NO_DEFAULT_PATH
+    )
+  
+  If(ROOT_CONFIG_EXECUTABLE)
+    
+    String(REGEX REPLACE "(^.*)/bin/root-config" "\\1" test ${ROOT_CONFIG_EXECUTABLE}) 
+    Set(ENV{ROOTSYS} ${test})
+    Set(ROOTSYS ${test})
+
+    Execute_Process(COMMAND ${ROOT_CONFIG_EXECUTABLE} --version 
+      OUTPUT_VARIABLE ROOT_VERSION_STRING
+      )
+    Execute_Process(COMMAND ${ROOT_CONFIG_EXECUTABLE} --prefix
+      OUTPUT_VARIABLE ROOT_INSTALL_DIR
+      )
+    String(STRIP ${ROOT_VERSION_STRING} ROOT_VERSION_STRING)
+    String(STRIP ${ROOT_INSTALL_DIR} ROOT_INSTALL_DIR)
+
+
+    MESSAGE(STATUS "Looking for Root... - Found ${ROOT_INSTALL_DIR}/bin/root")
+    MESSAGE(STATUS "Looking for Root... - Found version is ${ROOT_VERSION_STRING} ")   
+    
+    # extract major, minor, and patch versions from
+    # the version string given by root-config
+    String(REGEX REPLACE "^([0-9]+)\\.[0-9][0-9]+\\/[0-9][0-9]+.*" "\\1" ROOT_VERSION_MAJOR "${ROOT_VERSION_STRING}")
+    String(REGEX REPLACE "^[0-9]+\\.([0-9][0-9])+\\/[0-9][0-9]+.*" "\\1" ROOT_VERSION_MINOR "${ROOT_VERSION_STRING}")
+    String(REGEX REPLACE "^[0-9]+\\.[0-9][0-9]+\\/([0-9][0-9]+).*" "\\1" ROOT_VERSION_PATCH "${ROOT_VERSION_STRING}")
+
+    # compute overall version numbers which can be compared at once
+    Math(EXPR req_vers "${ROOT_FIND_VERSION_MAJOR}*10000 + ${ROOT_FIND_VERSION_MINOR}*100 + ${ROOT_FIND_VERSION_PATCH}")
+    Math(EXPR found_vers "${ROOT_VERSION_MAJOR}*10000 + ${ROOT_VERSION_MINOR}*100 + ${ROOT_VERSION_PATCH}")
+
+    Set(ROOT_Version ${found_vers})
+    Set(ROOT_VERSION_NUMBER ${found_vers})
+
+    If(found_vers LESS req_vers)
+      Set(ROOT_FOUND FALSE)
+      Set(ROOT_INSTALLED_VERSION_TOO_OLD TRUE)
+    Else(found_vers LESS req_vers)
+      Set(ROOT_FOUND TRUE)
+    EndIf(found_vers LESS req_vers)
+
+  Else(ROOT_CONFIG_EXECUTABLE)
+    Message(STATUS "Looking for Root... - Not found")
+    Message(FATAL_ERROR "ROOT not installed in the searchpath and ROOTSYS is not set. Please set ROOTSYS or add the path to your ROOT installation in the Macro FindROOT.cmake in the subdirectory cmake/modules.")
+  Endif(ROOT_CONFIG_EXECUTABLE)
+endif()
 
 If(ROOT_FOUND)
 
@@ -168,9 +207,13 @@ If(ROOT_FOUND)
     NO_DEFAULT_PATH
     )
 
-  #Include(ROOTMacros)
-  # Aliases for imported VMC packages ROOT dependencies
-  foreach(_root_dep VMC Core RIO Tree Rint Physics MathCore Thread Geom EG Eve Spectrum TreePlayer GenVector)
+  # Aliases for imported ROOT dependencies will probably need a lot more listed here eventually
+  set(_root_deps Core RIO Tree Rint Physics MathCore Thread Geom EG Eve Spectrum TreePlayer GenVector)
+  if(ROOT_vmc_FOUND)
+    list(APPEND _root_deps VMC)
+  endif()
+
+  foreach(_root_dep ${_root_deps})
     find_library(${_root_dep}_LIB ${_root_dep} PATHS ${ROOT_LIBRARY_DIR})
     if(${_root_dep}_LIB)
       add_library(${_root_dep} SHARED IMPORTED GLOBAL)
