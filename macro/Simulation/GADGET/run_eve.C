e1ce2b38728eaaafcc5cb151aa15371092c59088
Adam Anthony <anthonya@nscl.msu.edu>
2022-06-02 22:58:48 -0400

    Update GADGETII run_eve macro

--- macro/Simulation/GADGET/run_eve.C
+++ macro/Simulation/GADGET/run_eve.C
@@ -6,14 +6,18 @@ void run_eve(TString InputDataFile = "output_digi.root", TString OutputDataFile 
    fLogger->SetLogVerbosityLevel("MEDIUM");
    TString dir = getenv("VMCWORKDIR");
    TString geoFile = "GADGET_II_geomanager.root";
+   TString mapFile = "LookupGADGET08232021.xml";
 
    TString InputDataPath = dir + "/macro/" + unpackDir + InputDataFile;
    TString OutputDataPath = dir + "/macro/" + unpackDir + OutputDataFile;
    TString GeoDataPath = dir + "/geometry/" + geoFile;
+   TString mapDir = dir + "/scripts/" + mapFile;
 
    FairRunAna *fRun = new FairRunAna();
-   fRun->SetInputFile(InputDataPath);
-   fRun->SetOutputFile(OutputDataPath);
+   FairRootFileSink *sink = new FairRootFileSink(OutputDataFile);
+   FairFileSource *source = new FairFileSource(InputDataFile);
+   fRun->SetSource(source);
+   fRun->SetSink(sink);
    fRun->SetGeomFile(GeoDataPath);
 
    FairRuntimeDb *rtdb = fRun->GetRuntimeDb();
@@ -28,7 +32,9 @@ void run_eve(TString InputDataFile = "output_digi.root", TString OutputDataFile 
    eve->Set3DHitStyleBox();
    eve->SetMultiHit(100); // Set the maximum number of multihits in the visualization
    eve->SetSaveTextData();
-   eve->SelectDetectorId(kGADGETII);
+   auto fMap = std::make_shared<AtGadgetIIMap>();
+   fMap->ParseXMLMap(mapDir.Data());
+   eve->SetMap(fMap);
    eve->UnpackHoughSpace();
 
    eveMan->AddTask(eve);
