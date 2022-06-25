0fe82db982e348f6340775cc7aae6dac28f20396
Bergen Kendziorski <kendzi23@msu.edu>
2022-06-25 13:47:38 -0400

    initial changes to window fitting. Primarily testing resolving first conflict

--- macro/e12014/bergen/window_fitting.C
+++ macro/e12014/bergen/window_fitting.C
@@ -1,3 +1,5 @@
+#ifndef WINDOW_C
+#define WINDOW_C
 #include "mira.C"
 
 /*For fitting pad traces near window
@@ -6,19 +8,36 @@
  */
 void window_fitting()
 {
+   //runs automatically to load in run. For right now, we always use this run
    loadRun("/mnt/analysis/e12014/TPC/unpackedReducedFiltered/run_0210.root");
-   loadEvent(0);
-   TH1F *padHist = loadPad(155);
-   if (padHist != nullptr)
-      padHist->Draw("hist");
-   // need to load data into a histogram object
-   // assume hist
+
 }
 
-double fitwindow(double *x, double *par)
+void loadData(ULong64_t eventNumber, int padNumber)
 {
+   //uses functions from mira.C to load in histogram for given event and padnumber
+   loadEvent(eventNumber);
+   TH1F *padHist = loadPad(padNumber);
+   if (padHist != nullptr)
+      padHist->Draw("hist");
+}
+
+double fitEquation(double *x, double *par)
+{
+   //sets up equation used for the fitting
    double fitval = par[1] / (TMath::Exp((x[0] - par[0]) / par[3]) + 1) - par[2];
    return fitval;
+}
+
+void runFit(int left_bound, int right_bound, int xloc, int jump_height, int init_height)
+{
+   //5 integer inputs. First 2 are the time-bucket bounds. 3rd is x-location of jump, 4th is jump height, and 5th is height before the jump (all approximated)
+   TF1 *func = new TF1("func", fitEquation, left_bound, right_bound, 4);
+
+   func->SetParameters(xloc, jump_height, init_height, 1);
+   func->SetParNames("xloc", "jump_height", "init_height", "temp");
+
+   func->Fit("fitEquaion")
 }
 
 /* I think these need to be in a function to compile properly.
@@ -26,12 +45,6 @@ double fitwindow(double *x, double *par)
  * (like TF1* func = ...) but you cannot use them and call functions on them
  * (there is no injection popint for the code, so like the func->Set... needs to be in window_fitting() or another
  * function. to run */
-//-5 and 5 are range. Change these to desired.
-// For now put in function where these are inputs
-TF1 *func = new TF1("fit", fitwindow, -5, 5, 4);
 
-/*
-// See notepad for best parameters. Likely also function inputs (0-2 at least)
-func->SetParameters(450, 100, 20, 1);
-func->SetParNames("xloc", "jump_height", "init_height", "temp");
-*/
+
+#endif //ifndef WINDOW_C
