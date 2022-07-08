00320e7d6425c04900fe5765a4f10186d0b9ce67
Arian Andalib <andalib@nscl.msu.edu>
2022-07-08 09:59:49 -0400

    improved SeGA analysis

--- macro/Simulation/SeGA/Sim_analysis.C
+++ macro/Simulation/SeGA/Sim_analysis.C
@@ -1,7 +1,7 @@
 // Generic analysis for HELIOS MC simulation
 // Y. Ayyad ayyadlim@frib.msu.edu 12/30/2020
 
-void Sim_analysis(Int_t num_ev=10000)
+void Sim_analysis(Int_t num_ev=100000)
 {
 
     TString mcFileNameHead = "./data/SeGA";
@@ -25,14 +25,13 @@ void Sim_analysis(Int_t num_ev=10000)
     if(nEvents>num_ev) nEvents=num_ev;
 
     //Histograms
-    TH2D *Eloss_vs_Z = new TH2D("Eloss_vs_Z","ELoss_vs_Z",1000,0,-1000,1000,0,10);
-    TH2D *ElossTarget_vs_Z = new TH2D("ElossTarget_vs_Z","ELossTarget_vs_Z",1000,0,-1000,1000,0,10);
-    TH2D *Recoil_XY = new TH2D("Recoil_XY","Recoil_XY",1000,-50.0,50.0,1000,-50.0,50.0);
-    TH2D *ThetaLabSim_vs_Z = new TH2D("ThetaLabSim_vs_Z","ThetaLabSim_vs_Z",1000,0,-1000,1000,-180,180);
+    TH1D *Energy_loss= new TH1D("Energy_loss","Energy_loss",1000,0,10);
+    
 
       TCanvas *c1 = new TCanvas();
-    c1->Divide(2,2);
+    
     c1->Draw();
+Double_t Count =0.0;
 
     for(Int_t iEvent=0; iEvent<nEvents; iEvent++)
     {
@@ -40,72 +39,54 @@ void Sim_analysis(Int_t num_ev=10000)
 	tree->GetEvent(iEvent);
         // tree -> GetEntry(iEvent);
         Int_t n = pointArray -> GetEntries();
-        std::cout<<" ---- Event Number : "<<iEvent<<std::endl;
-        double zpos_sca = 0.0;
-        double energyLoss_sca = 0.0;
-        double energyLossTarget_sca = 0.0;
-        double zpos_rec = 0.0;
-        double xpos_rec = 0.0;
-        double ypos_rec = 0.0;
-        double energyLoss_rec = 0.0;
-	double angle_sca = 0.0;
-	double energy_sca = 0.0;
-
+       // std::cout<<" ---- Event Number : "<<iEvent<<std::endl;
+        
+        double energyLoss = 0.0;
+        
            for(Int_t i=0; i<n; i++) {
 
             point = (AtMCPoint*) pointArray -> At(i);
             auto VolName=point->GetVolName();
+
             auto trackID = point -> GetTrackID();
-            //std::cout<<" Volume Name : "<<VolName<<std::endl;
+           // std::cout<<" Volume Name : "<<VolName<<std::endl;
             //std::cout<<" Track ID : "<<trackID<<std::endl;
 
-             if(trackID==1) //Scattered proton
-	     {
-              if(VolName!="germanium"){
-	       std::cout<<" Volume Name : "<<VolName<<std::endl;
+            
+	       /*std::cout<<" Volume Name : "<<VolName<<std::endl;
                std::cout<<" Track ID : "<<trackID<<std::endl;
-               std::cout<<" Point number : "<<i<<std::endl;
-	       energyLoss_sca+=( point -> GetEnergyLoss() )*1000;//MeV
-	       zpos_sca=point->GetZ()*10;
-               angle_sca=point->GetAIni();
-	       std::cout<<" Energy Loss "<<energyLoss_sca<<"  Z pos "<<zpos_sca<<" Point ID : "<<i<<"\n";
-               }else if(VolName=="germanium"){
-                 
-                 energyLossTarget_sca+=( point -> GetEnergyLoss() )*1000;//MeV;
-               }
+               std::cout<<" Point number : "<<i<<std::endl;*/
+	       energyLoss+=( point -> GetEnergyLoss() )*1000;//MeV
+		Count+=1;
 
-	     }
+	
 
-             if(trackID==0) //Heavy recoil
-	     {
-	      if(VolName!="germanium"){
-	       std::cout<<" Volume Name : "<<VolName<<std::endl;
-               std::cout<<" Track ID : "<<trackID<<std::endl;
-               std::cout<<" Point number : "<<i<<std::endl;
-	       energyLoss_rec+=( point -> GetEnergyLoss() )*1000;//MeV
-	       zpos_rec=point->GetZ()*10;
-               xpos_rec=point->GetX()*10;
-               ypos_rec=point->GetY()*10;
-	       //std::cout<<" Energy Loss "<<energyLoss_rec<<"  Z pos "<<zpos_rec<<" Point ID : "<<i<<"\n";
-              }
+	       
 	     }
 				
 
-           }
+         if (energyLoss != 0.0){  
 
-	Eloss_vs_Z->Fill(zpos_sca,energyLoss_sca);
-        Recoil_XY->Fill(xpos_rec,ypos_rec);
-        ThetaLabSim_vs_Z->Fill(zpos_sca,angle_sca);
-
-        ElossTarget_vs_Z->Fill(zpos_sca,energyLossTarget_sca);
+	Energy_loss->Fill(energyLoss);};
+	
 
     }
-    
+// Photo Peak efficeny
+Double_t momentum = 0.005;
+Double_t photopeakcount =0.0;
+for(Int_t i=0;i<1000;i++){
+	Double_t cur = Energy_loss->GetBinContent(i);
+	if(cur > photopeakcount){		
+		photopeakcount = cur;
+}
+};
+Double_t eff = (photopeakcount*100/Count);
+std::cout<<"Photo peak efficency: "<<eff<<std::endl;
+
+   // std::sort(x,x+20);
     c1->cd(1);
-    Eloss_vs_Z->Draw("zcol");
-    c1->cd(2);
-    Recoil_XY->Draw("zcol");
-    c1->cd(3);
-    ElossTarget_vs_Z->Draw("zcol");
+	
+    Energy_loss->Draw();
+
 
 }
