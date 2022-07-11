174238dc5b57b1b195c684222bfbc364a5d15bb8
Curtis Hunt <huntc@flagtail.nscl.msu.edu>
2022-07-11 15:31:00 -0400

    Add additional options for beam simulation.
    Add option to disable reaction in simulation. Add method to set beam
    emittance in simulation.

--- AtGenerators/AtTPCIonGenerator.cxx
+++ AtGenerators/AtTPCIonGenerator.cxx
@@ -169,6 +169,25 @@ void AtTPCIonGenerator::SetEmittance()
 }
 //_________________________________________________________________________
 
+// -----   Private method SetBeamOrigin   ---------------------------------
+void AtTPCIonGenerator::SetBeamOrigin()
+{
+    double pi = 2 * asin(1.0);
+
+    Double_t radius = std::clamp(gRandom->Gaus(0, fR / 3), 0.0, fR);
+    Double_t phi_R = gRandom->Uniform(0, 2 * pi); 
+    fVx = radius * cos(phi_R);
+    fVy = radius * sin(phi_R);
+
+    Double_t theta = gRandom->Uniform(0, fTheta);
+    Double_t pr = fPz * sin(theta); 
+    fPz *= cos(theta);
+    fPx = pr * cos(phi_R);
+    fPy = pr * sin(phi_R);
+
+}
+//_________________________________________________________________________
+
 // -----   Public method ReadEvent   --------------------------------------
 Bool_t AtTPCIonGenerator::ReadEvent(FairPrimaryGenerator *primGen)
 {
@@ -200,6 +219,9 @@ Bool_t AtTPCIonGenerator::ReadEvent(FairPrimaryGenerator *primGen)
       SetEmittance(); // parameters: fWhmFocus, fDiv, fZFocus, fRHole, fPx, fPy, fPz
       // changes: fVx, fVy, fVz, fPx, fPy, fPz, d2HeVtx
       break;
+   case 3:
+      SetBeamOrigin();
+      break;
    default:
       fVx = 0.0;
       fVy = 0.0;
@@ -215,9 +237,12 @@ Bool_t AtTPCIonGenerator::ReadEvent(FairPrimaryGenerator *primGen)
    AtVertexPropagator::Instance()->IncBeamEvtCnt();
 
    if (AtVertexPropagator::Instance()->GetBeamEvtCnt() % 2 != 0) {
-      Double_t Er = gRandom->Uniform(0., fMaxEnLoss);
-      AtVertexPropagator::Instance()->SetRndELoss(Er);
-      // std::cout << cGREEN << " Random Energy AtTPCIonGenerator : " << Er << cNORMAL << std::endl;
+      if (fDoReact) {
+          Double_t Er = gRandom->Uniform(0., fMaxEnLoss);
+          AtVertexPropagator::Instance()->SetRndELoss(Er);
+        // std::cout << cGREEN << " Random Energy AtTPCIonGenerator : " << Er << cNORMAL << std::endl;
+      }
+      else AtVertexPropagator::Instance()->SetRndELoss(std::numeric_limits<double>::max());
    }
 
    for (Int_t i = 0; i < fMult; i++)
