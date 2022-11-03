ed2d20f69f9de8fba5e8defddf685101f7b19c98
Adam Anthony <anthonya@nscl.msu.edu>
2022-11-03 16:40:22 -0400

    Update tests for unpacking AT-TPC data with GRAW

--- macro/tests/AT-TPC/runTests.sh
+++ macro/tests/AT-TPC/runTests.sh
@@ -4,7 +4,9 @@ GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 
 # Ordered list of tests to run
-tests=("run_sim_attpc.C" "run_digi_attpc.C" "run_eve_sim.C" "run_unpack_attpc.C" "run_eve.C") 
+tests=("run_sim_attpc.C" "run_digi_attpc.C" "run_eve_sim.C" "run_unpack_attpc.C" "run_unpack_graw.C" "run_eve.C") 
+
+./symLink.sh 
 
 for i in ${!tests[@]}; do
     echo "Test $i: running ${tests[$i]}"
