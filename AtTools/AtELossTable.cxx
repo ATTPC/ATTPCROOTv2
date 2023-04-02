233b90d3157d9afc3a950111c21fcf23ce512e71
Adam Anthony <anthonya@nscl.msu.edu>
2023-04-02 15:10:00 -0400

    Add ability to load LISE tables for energy loss

--- AtTools/AtELossTable.cxx
+++ AtTools/AtELossTable.cxx
@@ -10,7 +10,11 @@ namespace AtTools {
 
 void AtELossTable::LoadTable(const std::vector<double> &energy, const std::vector<double> &dEdX)
 {
-   LOG(info) << "Loading dE/dX from " << energy.front() << " MeV to " << energy.back() << " MeV ";
+   if (energy.size() > 0)
+      LOG(info) << "Loading dE/dX from " << energy.front() << " MeV to " << energy.back() << " MeV ";
+   else
+      LOG(error) << "Passed table was empty!";
+
    std::vector<double> dXdE;
    for (auto &elem : dEdX)
       dXdE.push_back(1 / elem);
@@ -160,16 +164,54 @@ void AtELossTable::LoadSrimTable(std::string fileName)
    LoadTable(energy, dEdX);
 }
 
-double AtELossTable::GetUnitConversion(const std::string &unit)
+void AtELossTable::LoadLiseTable(std::string fileName, double mass, double density, int column)
 {
-   if (unit == "eV")
-      return 1e-6;
-   if (unit == "keV")
-      return 1e-3;
-   if (unit == "MeV")
-      return 1e-0;
-   if (unit == "GeV")
-      return 1e3;
-   return 0;
+   std::ifstream file(fileName);
+   if (!file.is_open())
+      LOG(fatal) << "Failed to open SRIM file " << fileName;
+
+   std::vector<double> energy;
+   std::vector<double> dEdX;
+   fDensity = fabs(density);
+
+   while (!file.eof()) {
+      // Get the current line
+      std::string line;
+      std::getline(file, line);
+      auto tokens = SplitString(line, '\t');
+      LOG(debug) << "Processing line " << line;
+
+      try {
+         //  If this is part of the table, grab it and continue
+         double en = std::stod(tokens.at(0 + column * 2)) * mass;
+         double dedx = std::stod(tokens.at(1 + column * 2));
+
+         if (density > 0) // dEdX is in units MeV/(mg/cm^2)
+            dedx *= (100 * density);
+         else // dEdX is in units MeV/um
+            dedx *= 1000;
+
+         LOG(debug) << "En: " << en << " dEdX " << dedx;
+         // If we made it this far then we have a valid table entry
+         energy.push_back(en);
+         dEdX.push_back(dedx);
+      } catch (...) {
+      }
+   }
+
+   LoadTable(energy, dEdX);
 }
+
+   double AtELossTable::GetUnitConversion(const std::string &unit)
+   {
+      if (unit == "eV")
+         return 1e-6;
+      if (unit == "keV")
+         return 1e-3;
+      if (unit == "MeV")
+         return 1e-0;
+      if (unit == "GeV")
+         return 1e3;
+      return 0;
+   }
 } // namespace AtTools
--- AtTools/AtELossTable.h
+++ AtTools/AtELossTable.h
@@ -20,6 +20,15 @@ public:
    /// Set error in particle range when calculating energy losses.
    void SetDistanceError(double val) { fDistErr = val; }
    void LoadSrimTable(std::string fileName);
+   /**
+    * Load energy loss table from LISE++ (export of stopping power graph to file).
+    * @param[in] fileName Name of text file.
+    * @param[in] mass Mass of the fragment in units of u.
+    * @param[in] density Density of target in g/cm^3. If positive, units of table are assumed to be MeV/mg/cm^2. If not
+    * positive units of the table are assumed to be MeV/um and the target density is taken to be |density|.
+    *@param[in] column The column in the table to use as the energy model (default LISE is 2).
+    */
+   void LoadLiseTable(std::string fileName, double mass, double density, int column = 2);
 
    virtual double GetdEdx(double energy) const override;
    virtual double GetRange(double energyIni, double energyFin = 0) const override;
