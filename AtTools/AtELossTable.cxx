f71f2d03eecb4063396eef824884b87458dfd45e
Adam Anthony <anthonya@nscl.msu.edu>
2023-04-02 10:19:23 -0400

    Speed up energy loss calulations.
    Energy loss converges in ~2-4 iterations rather than 20-30
    iterations. Switched from a binary search to what is in spirit a
    version of Newton's method.

--- AtTools/AtELossTable.cxx
+++ AtTools/AtELossTable.cxx
@@ -1,4 +1,9 @@
 #include "AtELossTable.h"
+
+#include <FairLogger.h>
+
+#include <iostream>
+
 namespace AtTools {
 
 AtELossTable::AtELossTable(const std::vector<double> &energy, const std::vector<double> &dEdX, double density)
@@ -28,6 +33,33 @@ double AtELossTable::GetEnergy(double energyIni, double distance) const
 {
    if (distance == 0)
       return energyIni;
+   if (energyIni < 1e-6 || GetRange(energyIni) < distance)
+      return 0.;
+
+   int maxIt = 100;
+
+   double guessEnergy = energyIni - GetdEdx(energyIni) * distance;
+   for (int i = 0; i < maxIt; ++i) {
+
+      double range = GetRange(energyIni, guessEnergy);
+      if (fabs(range - distance) < fDistErr) {
+         LOG(debug) << "Energy converged in " << i + 1 << " iterations.";
+         return guessEnergy;
+      }
+      LOG(debug) << "guessE: " << guessEnergy << " d: " << distance << " R: " << range
+                 << " dEdX: " << GetdEdx(guessEnergy) << " dEnergy: " << GetdEdx(guessEnergy) * (distance - range);
+      // Update guess energy using what is essentially Newton's method
+      guessEnergy += GetdEdx(guessEnergy) * (range - distance);
+   }
+
+   LOG(error) << "Energy calculation failed to converge in " << maxIt << " iterations!";
+   return -1;
+}
+
+double AtELossTable::GetEnergyOld(double energyIni, double distance) const
+{
+   if (distance == 0)
+      return energyIni;
    if (energyIni < 1e-6)
       return 0.;
 
@@ -43,8 +75,10 @@ double AtELossTable::GetEnergy(double energyIni, double distance) const
    double guessEnergy = energyIni / 2.0;
    double range = GetRange(energyIni, guessEnergy);
    double distErr = 1.0e-4; // in mm
+   int numIt = 1;
 
    while (fabs(range - distance) > distErr) {
+      numIt++;
       // We went too far
       if (range > distance) {
          lowGuess = guessEnergy;
@@ -57,6 +91,7 @@ double AtELossTable::GetEnergy(double energyIni, double distance) const
       range = GetRange(energyIni, guessEnergy);
    }
 
+   LOG(info) << "Energy converged in " << numIt << " iterations.";
    return guessEnergy;
 }
 
--- AtTools/AtELossTable.h
+++ AtTools/AtELossTable.h
@@ -11,8 +11,13 @@ class AtELossTable : public AtELossModel {
 protected:
    tk::spline fdXdE;
 
+   double fDistErr{1e-4};
+
 public:
    AtELossTable(const std::vector<double> &energy, const std::vector<double> &dEdX, double density = 0);
+
+   /// Set error in particle range when calculating energy losses.
+   void SetDistanceError(double val) { fDistErr = val; }
 
    virtual double GetdEdx(double energy) const override;
    virtual double GetRange(double energyIni, double energyFin = 0) const override;
@@ -21,6 +26,8 @@ public:
       return energyIni - GetEnergy(energyIni, distance);
    }
    virtual double GetEnergy(double energyIni, double distance) const override;
+
+   [[deprecated]] double GetEnergyOld(double energyIni, double distance) const;
 };
 
 } // namespace AtTools
