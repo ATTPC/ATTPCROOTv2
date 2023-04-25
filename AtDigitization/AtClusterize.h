7024dc2f45e817138597248308a7fd04738a6e7f
Adam Anthony <anthonya@nscl.msu.edu>
2023-04-25 18:50:20 -0400

    Run clang format

--- AtDigitization/AtClusterize.h
+++ AtDigitization/AtClusterize.h
@@ -34,8 +34,8 @@ protected:
    double fCoefL{};       //!< Longitudinal diffusion coefficient. [cm^2/us]
    double fDetPadPlane{}; //!< Position of the pad plane with respect to the entrance [mm]
 
-   static thread_local XYZPoint fPrevPoint;  //!< The previous point we recorded charge.
-   static thread_local int fTrackID;         //!< The current track ID
+   static thread_local XYZPoint fPrevPoint; //!< The previous point we recorded charge.
+   static thread_local int fTrackID;        //!< The current track ID
 
 public:
    std::vector<SimPointPtr> ProcessEvent(const TClonesArray &fMCPointArray);
