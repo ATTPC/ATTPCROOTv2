9b51fda1adb0109eae1841da26f9b365aa25a20b
Adam Anthony <anthonya@nscl.msu.edu>
2023-04-25 18:48:54 -0400

    Fix bug where all threads were sampling the same numbers

--- AtReconstruction/AtFitter/ParameterDistributions/AtParameterDistribution.cxx
+++ AtReconstruction/AtFitter/ParameterDistributions/AtParameterDistribution.cxx
@@ -3,19 +3,20 @@ namespace MCFitter {
 
 thread_local std::unique_ptr<std::mt19937> AtParameterDistribution::fRand = nullptr;
 
-AtParameterDistribution::AtParameterDistribution(double mean, double spread, long seed) : fMean(mean), fSpread(spread)
+AtParameterDistribution::AtParameterDistribution(double mean, double spread, long seed)
+   : fMean(mean), fSpread(spread), fSeed(seed)
 {
-   // Use a random seed if a seed was not passed
-   if (fSeed == 0) {
-      std::random_device rd;
-      fSeed = rd();
-   }
 }
 
 double AtParameterDistribution::Sample()
 {
-   if (fRand == nullptr)
+   if (fRand == nullptr) {
+      if (fSeed == 0) {
+         std::random_device rd;
+         fSeed = rd();
+      }
       fRand = std::make_unique<std::mt19937>(fSeed);
+   }
    return fMean + fSpread * SampleSpread();
 }
 } // namespace MCFitter
