11f6147f33ad71d6de199b130e3f2223a3cf2340
Adam Anthony <anthonya@nscl.msu.edu>
2023-04-25 18:56:15 -0400

    Actualy fix seeding bug

--- AtReconstruction/AtFitter/ParameterDistributions/AtParameterDistribution.cxx
+++ AtReconstruction/AtFitter/ParameterDistributions/AtParameterDistribution.cxx
@@ -1,4 +1,8 @@
 #include "AtParameterDistribution.h"
+
+#include <FairLogger.h>
+
+#include <thread>
 namespace MCFitter {
 
 thread_local std::unique_ptr<std::mt19937> AtParameterDistribution::fRand = nullptr;
@@ -10,12 +14,14 @@ AtParameterDistribution::AtParameterDistribution(double mean, double spread, lon
 
 double AtParameterDistribution::Sample()
 {
+   long seed = fSeed;
    if (fRand == nullptr) {
-      if (fSeed == 0) {
+      if (seed == 0) {
          std::random_device rd;
-         fSeed = rd();
+         seed = rd();
       }
-      fRand = std::make_unique<std::mt19937>(fSeed);
+      LOG(info) << "Seeding thread with " << seed;
+      fRand = std::make_unique<std::mt19937>(seed);
    }
    return fMean + fSpread * SampleSpread();
 }
