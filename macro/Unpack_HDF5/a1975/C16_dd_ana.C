7ba9c9a10d3951008ec05c71630426f56db0dc4f
Yassid <ayyadlim@nscl.msu.edu>
2023-06-23 19:56:07 +0200

    Finished debugging DAQ merging

--- macro/Unpack_HDF5/a1975/C16_dd_ana.C
+++ macro/Unpack_HDF5/a1975/C16_dd_ana.C
@@ -76,6 +76,7 @@ void C16_dd_ana()
    TH2F *angle_vs_momentum = new TH2F("angle_vs_momentum", "angle_vs_momentum", 720, 0, 179, 1000, 0, 2.0);
 
    TH1F *HQval = new TH1F("HQval", "HQval", 600, -5, 55);
+   TH1F *HQvalp = new TH1F("HQvalp", "HQvalp", 600, -5, 55);
    TH2F *QvsEb = new TH2F("QvsEb", "QvsEb", 1000, -5, 15, 300, 0, 300);
    TH2F *QvsZpos = new TH2F("QvsZpos", "QvsZpos", 1000, -10, 50, 200, -100, 100);
 
@@ -87,36 +88,35 @@ void C16_dd_ana()
    cutg->SetVarY("");
    cutg->SetTitle("Graph");
    cutg->SetFillStyle(1000);
-   cutg->SetPoint(0, 238.8968, 0.9658209);
-   cutg->SetPoint(1, 829.8962, 0.8813731);
-   cutg->SetPoint(2, 1479.163, 0.6826119);
-   cutg->SetPoint(3, 1803.797, 0.5775672);
-   cutg->SetPoint(4, 2544.627, 0.4797313);
-   cutg->SetPoint(5, 3976.344, 0.3808657);
-   cutg->SetPoint(6, 5341.469, 0.3067164);
-   cutg->SetPoint(7, 6806.482, 0.2737612);
-   cutg->SetPoint(8, 8829.198, 0.2315373);
-   cutg->SetPoint(9, 10693.76, 0.205791);
-   cutg->SetPoint(10, 13956.74, 0.1800448);
-   cutg->SetPoint(11, 15255.28, 0.171806);
-   cutg->SetPoint(12, 15546.61, 0.1058955);
-   cutg->SetPoint(13, 14922.32, 0.05955223);
-   cutg->SetPoint(14, 12616.59, 0.0873582);
-   cutg->SetPoint(15, 10543.93, 0.09868656);
-   cutg->SetPoint(16, 9769.803, 0.1131045);
-   cutg->SetPoint(17, 8629.424, 0.1120746);
-   cutg->SetPoint(18, 7114.467, 0.1120746);
-   cutg->SetPoint(19, 4858.681, 0.1872537);
-   cutg->SetPoint(20, 3443.612, 0.2346269);
-   cutg->SetPoint(21, 2544.627, 0.289209);
-   cutg->SetPoint(22, 1853.74, 0.3417313);
-   cutg->SetPoint(23, 1204.473, 0.4457463);
-   cutg->SetPoint(24, 896.4877, 0.4992985);
-   cutg->SetPoint(25, 563.5303, 0.6022836);
-   cutg->SetPoint(26, 89.06599, 0.7711791);
-   cutg->SetPoint(27, 222.249, 0.9668508);
-   cutg->SetPoint(28, 230.5729, 0.9668508);
-   cutg->SetPoint(29, 238.8968, 0.9658209);
+   cutg->SetPoint(0, 10146.09, 0.229444);
+   cutg->SetPoint(1, 15745.65, 0.1844776);
+   cutg->SetPoint(2, 16204.63, 0.09800372);
+   cutg->SetPoint(3, 13122.9, 0.08070895);
+   cutg->SetPoint(4, 9949.381, 0.1003097);
+   cutg->SetPoint(5, 8165.915, 0.1037686);
+   cutg->SetPoint(6, 6316.88, 0.1141455);
+   cutg->SetPoint(7, 4310.48, 0.156806);
+   cutg->SetPoint(8, 3208.928, 0.2063843);
+   cutg->SetPoint(9, 1871.328, 0.2421269);
+   cutg->SetPoint(10, 1491.03, 0.2963172);
+   cutg->SetPoint(11, 900.9125, 0.3989328);
+   cutg->SetPoint(12, 743.5479, 0.4427463);
+   cutg->SetPoint(13, 586.1832, 0.5880224);
+   cutg->SetPoint(14, 284.5676, 0.7921007);
+   cutg->SetPoint(15, 205.8853, 0.9846493);
+   cutg->SetPoint(16, 153.4304, 1.208328);
+   cutg->SetPoint(17, 232.1127, 1.231388);
+   cutg->SetPoint(18, 428.8185, 1.215246);
+   cutg->SetPoint(19, 704.2067, 1.037687);
+   cutg->SetPoint(20, 782.889, 0.8589739);
+   cutg->SetPoint(21, 1373.007, 0.5949403);
+   cutg->SetPoint(22, 2146.716, 0.464653);
+   cutg->SetPoint(23, 3733.477, 0.3458955);
+   cutg->SetPoint(24, 5307.123, 0.2893992);
+   cutg->SetPoint(25, 6736.519, 0.2651866);
+   cutg->SetPoint(26, 8506.872, 0.2409739);
+   cutg->SetPoint(27, 10198.54, 0.228291);
+   cutg->SetPoint(28, 10146.09, 0.229444);
 
    // protons
    TCutG *cutp = new TCutG("CUTP", 30);
@@ -171,11 +171,12 @@ void C16_dd_ana()
    Double_t m_C13 = 13.00335484 * 931.49401;
    Double_t m_C12 = 12.00 * 931.49401;
    Double_t m_C16 = 16.0147 * 931.49401;
+   Double_t m_C17 = 17.0226 * 931.49401;
 
    Double_t m_a = 4.00260325415 * 931.49401;
    Double_t m_O16 = 15.99491461956 * 931.49401;
 
-   Double_t Ebeam_buff = 192.0;
+   Double_t Ebeam_buff = 154.0; // 192.0;
    Double_t m_b;
    Double_t m_B;
 
@@ -194,8 +195,8 @@ void C16_dd_ana()
    filepairs.push_back(std::make_pair("run_0014.root","run_0014_FRIB_sorted.root"));
    filepairs.push_back(std::make_pair("run_0015.root","run_0015_FRIB_sorted.root"));
    filepairs.push_back(std::make_pair("run_0016.root","run_0016_FRIB_sorted.root"));
-   filepairs.push_back(std::make_pair("run_0016.root", "run_0017_FRIB_sorted.root"));
-   filepairs.push_back(std::make_pair("run_0016.root", "run_0018_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0017.root", "run_0017_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0018.root", "run_0018_FRIB_sorted.root"));
    filepairs.push_back(std::make_pair("run_0019.root", "run_0019_FRIB_sorted.root"));
    filepairs.push_back(std::make_pair("run_0020.root", "run_0020_FRIB_sorted.root"));
    filepairs.push_back(std::make_pair("run_0021.root", "run_0021_FRIB_sorted.root"));
@@ -222,6 +223,45 @@ void C16_dd_ana()
    filepairs.push_back(std::make_pair("run_0045.root", "run_0045_FRIB_sorted.root"));
    filepairs.push_back(std::make_pair("run_0046.root", "run_0046_FRIB_sorted.root"));
    filepairs.push_back(std::make_pair("run_0048.root", "run_0048_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0057.root", "run_0057_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0058.root", "run_0058_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0059.root", "run_0059_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0060.root", "run_0060_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0062.root", "run_0062_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0063.root", "run_0063_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0064.root", "run_0064_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0065.root", "run_0065_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0066.root", "run_0066_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0067.root", "run_0067_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0068.root", "run_0068_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0069.root", "run_0069_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0070.root", "run_0070_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0071.root", "run_0071_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0072.root", "run_0072_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0073.root", "run_0073_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0074.root", "run_0074_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0076.root", "run_0076_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0077.root", "run_0077_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0078.root", "run_0078_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0079.root", "run_0079_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0080.root", "run_0080_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0082.root", "run_0082_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0083.root", "run_0083_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0084.root", "run_0084_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0086.root", "run_0086_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0087.root", "run_0087_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0088.root", "run_0088_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0089.root", "run_0089_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0091.root", "run_0091_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0092.root", "run_0092_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0093.root", "run_0093_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0094.root", "run_0094_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0095.root", "run_0095_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0096.root", "run_0096_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0097.root", "run_0097_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0098.root", "run_0098_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0099.root", "run_0099_FRIB_sorted.root"));
+   filepairs.push_back(std::make_pair("run_0100.root", "run_0100_FRIB_sorted.root"));
 
    for (auto iFile : filepairs) {
 
@@ -334,8 +374,8 @@ void C16_dd_ana()
                Double_t theta = track.GetGeoTheta();
                Double_t rad = track.GetGeoRadius();
 
-               if (theta * TMath::RadToDeg() < 90.0)
-                  continue;
+               // if (theta * TMath::RadToDeg() > 90.0)
+               //  continue;
 
                Double_t B_f = 2.85;
 
@@ -344,6 +384,9 @@ void C16_dd_ana()
                Double_t Am = 2.0;
 
                GetEnergy(Am, 1.0, bro, ener);
+
+               if (ener * Am > 6.0)
+                  continue;
 
                // if (track.GetHitArray()->size() > 80)
                // angle_vs_energy->Fill(theta * TMath::RadToDeg(), ener * Am);
@@ -422,29 +465,37 @@ void C16_dd_ana()
                // if(ener*Am>8.0)
                //  continue;
 
-               // if (cutg->IsInside(eloss, bro)) { // Selection of protons
+               if (cutp->IsInside(eloss, bro)) {
+                  if (theta * TMath::RadToDeg() > 100.0) {
+                     angle_vs_energy_t->Fill(theta * TMath::RadToDeg(), ener * Am);
+                     auto [ex_energy_exp, theta_cm] = kine_2b(m_C16, m_d, m_p, m_C17, Ebeam_buff, theta, ener * Am);
+                     HQvalp->Fill(ex_energy_exp);
+                  }
+               }
 
-               angle_vs_energy->Fill(theta * TMath::RadToDeg(), ener * Am);
-               auto [ex_energy_exp, theta_cm] = kine_2b(m_C16, m_d, m_b, m_B, Ebeam_buff, theta, ener * Am);
+               if (cutg->IsInside(eloss, bro)) { // Selection of protons
 
-               HQval->Fill(ex_energy_exp);
+                  angle_vs_energy->Fill(theta * TMath::RadToDeg(), ener * Am);
+                  auto [ex_energy_exp, theta_cm] = kine_2b(m_C16, m_d, m_b, m_B, Ebeam_buff, theta, ener * Am);
 
-               // Excitation energy vs Beam energy
-               for (auto iEb = 0; iEb < 300; ++iEb) {
-                  auto [Qdep, theta_cm_qdep] = kine_2b(m_C16, m_d, m_b, m_B, iEb, theta, ener * Am);
-                  QvsEb->Fill(Qdep, iEb);
-                  //  }
+                  HQval->Fill(ex_energy_exp);
 
-                  // Rough vertex
-                  QvsZpos->Fill(ex_energy_exp, zpos / 10.0);
+                  // Excitation energy vs Beam energy
+                  for (auto iEb = 0; iEb < 300; ++iEb) {
+                     auto [Qdep, theta_cm_qdep] = kine_2b(m_C16, m_d, m_b, m_B, iEb, theta, ener * Am);
+                     QvsEb->Fill(Qdep, iEb);
+                     //  }
 
-               } // deuterons
+                     // Rough vertex
+                     QvsZpos->Fill(ex_energy_exp, zpos / 10.0);
 
-               bro_vs_eloss->Fill(eloss, bro);
-               bro_vs_dedx->Fill(dedx, bro);
+                  } // deuterons
 
-               angle_vs_energy_lr->Fill(theta * TMath::RadToDeg(), ener * Am);
-               //}
+                  bro_vs_eloss->Fill(eloss, bro);
+                  bro_vs_dedx->Fill(dedx, bro);
+
+                  angle_vs_energy_lr->Fill(theta * TMath::RadToDeg(), ener * Am);
+               }
             }
          }
 
@@ -508,16 +559,14 @@ void C16_dd_ana()
    QvsZpos->Draw();
 
    TCanvas *c_kn_el = new TCanvas();
-   // c_kn_el->Divide(1, 1);
-   // c_kn_el->cd(1);
+   c_kn_el->Divide(2, 1);
+   c_kn_el->cd(1);
    angle_vs_energy->Draw("colz");
    Kine_AngRec_EnerRec->SetLineColor(kRed);
    Kine_AngRec_EnerRec->Draw("SAME");
    Kine_1m1->Draw("SAME");
-   /*c_kn_el->cd(2);
-   bro_vs_dedx->Draw("colz");
-   c_kn_el->cd(3);
-   angle_vs_energy_t->Draw("colz");*/
+   c_kn_el->cd(2);
+   angle_vs_energy_t->Draw("colz");
 
    TCanvas *c_PID_eloss = new TCanvas();
    TCanvas *c_PID_dedx = new TCanvas();
@@ -530,11 +579,11 @@ void C16_dd_ana()
 
    angle_vs_energy->GetXaxis()->SetTitle("#theta (deg)");
    angle_vs_energy->GetYaxis()->SetTitle("E (MeV)");
-   angle_vs_energy->SetTitle("gate on p");
+   angle_vs_energy->SetTitle("gate on d");
 
    angle_vs_energy_t->GetXaxis()->SetTitle("#theta (deg)");
    angle_vs_energy_t->GetYaxis()->SetTitle("E (MeV)");
-   angle_vs_energy_t->SetTitle("gate on d");
+   angle_vs_energy_t->SetTitle("gate on p");
 
    bro_vs_dedx->GetXaxis()->SetTitle("Brho ");
    bro_vs_dedx->GetYaxis()->SetTitle("dE/dx (au)");
@@ -545,7 +594,11 @@ void C16_dd_ana()
    bro_vs_dedx->SetStats(0);
 
    TCanvas *c_ExEner = new TCanvas();
+   c_ExEner->Divide(2, 1);
+   c_ExEner->cd(1);
    HQval->Draw();
+   c_ExEner->cd(2);
+   HQvalp->Draw();
 
    TCanvas *c_IC = new TCanvas();
    henergyIC->Draw();
