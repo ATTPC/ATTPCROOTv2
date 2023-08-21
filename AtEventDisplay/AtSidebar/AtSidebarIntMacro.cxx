a8f27061878f3ef6599fd589ee913507feed7405
Curtis <cnhunt34@gmail.com>
2023-08-21 14:43:40 -0400

    Expanded AtMacroTask added AtSidebarIntMacro

---
+++ AtEventDisplay/AtSidebar/AtSidebarIntMacro.cxx
@@ -0,0 +1,18 @@
+#include "AtSidebarIntMacro.h"
+
+#include <FairLogger.h> // for endl, basic_ostream, cout, ostream
+
+ClassImp(AtSidebarIntMacro);
+
+void AtSidebarIntMacro::FillFrame()
+{
+   AddIntBox(fLabel, "RunFunction()", fMin, fMax);
+
+   SetIntNumber(fLabel, fStart);
+}
+
+void AtSidebarIntMacro::RunFunction() 
+{
+   auto value = GetIntNumber(fLabel);
+   fFunction(value);
+}
---
+++ AtEventDisplay/AtSidebar/AtSidebarIntMacro.h
@@ -0,0 +1,50 @@
+#ifndef ATSIDEBARINTMACRO_H
+#define ATSIDEBARINTMACRO_H
+
+#include "AtSidebarAddon.h"
+
+#include <Rtypes.h> // for THashConsistencyHolder, UInt_t, ClassDef...
+
+#include <GuiTypes.h> // for Pixel_t
+
+#include <functional>
+#include <string>
+#include <utility> // for move
+
+class TBuffer;
+class TClass;
+class TGWindow;
+class TMemberInspector;
+
+class AtSidebarIntMacro : public AtSidebarAddon {
+private:
+   using MacroFunction = std::function<void(int)>;
+   MacroFunction fFunction;
+
+   std::string fLabel;
+
+   int fMin{0};
+   int fMax{10};
+   int fStart{0};
+
+public:
+   AtSidebarIntMacro(const TGWindow *p = nullptr, UInt_t w = 1, UInt_t h = 1,
+                      UInt_t options = 0, Pixel_t back = GetDefaultFrameBackground())
+      : AtSidebarAddon(p, w, h, options, back)
+   {
+   }
+   ~AtSidebarIntMacro() { }
+
+   void FillFrame() override;
+
+   void RunFunction();
+
+   void SetBounds(int min, int max) { fMin = min; fMax = max; }
+   void SetFunction(MacroFunction function) { fFunction = std::move(function); }
+   void SetInitialValue(int value) { fStart = value; }
+   void SetLabel(std::string label) { fLabel = label; }
+
+   ClassDefOverride(AtSidebarIntMacro, 1);
+};
+
+#endif
