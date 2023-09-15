2db4f7086f75235c331378dc25f3890f7ec2df98
analysis-computer <analysis-computer>
2023-09-15 16:10:48 -0500

    Add initial analysis macros

---
+++ macro/Simulation/ATTPC/15C_d/run_digi_attpc.C
@@ -0,0 +1,84 @@
+bool reduceFunc(AtRawEvent *evt);
+
+void run_digi_attpc()
+{
+   TString inOutDir = "./data/";
+   TString outputFile = inOutDir + "output_digi.root";
+   TString scriptfile = "Lookup20150611.xml";
+   TString paramFile = "ATTPC.e20009_sim.par";
+
+   TString dir = getenv("VMCWORKDIR");
+
+   // TString mcFile = "./data/sim_attpc.root";
+   TString mcFile = inOutDir + "attpcsim.root";
+
+   // Create the full parameter file paths
+   TString digiParFile = dir + "/parameters/" + paramFile;
+   TString mapParFile = dir + "/scripts/" + scriptfile;
+
+   // -----   Timer   --------------------------------------------------------
+   TStopwatch timer;
+
+   // ------------------------------------------------------------------------
+
+   // __ Run ____________________________________________
+   FairRunAna *fRun = new FairRunAna();
+   FairFileSource *source = new FairFileSource(mcFile);
+   fRun->SetSource(source);
+   fRun->SetOutputFile(outputFile);
+
+   FairRuntimeDb *rtdb = fRun->GetRuntimeDb();
+   FairParAsciiFileIo *parIo1 = new FairParAsciiFileIo();
+   parIo1->open(digiParFile.Data(), "in");
+   rtdb->setFirstInput(parIo1);
+
+   // Create the detector map to pass to the simulation
+   auto mapping = std::make_shared<AtTpcMap>();
+   mapping->ParseXMLMap(mapParFile.Data());
+   mapping->GeneratePadPlane();
+   mapping->ParseInhibitMap("./data/inhibit.txt", AtMap::InhibitType::kTotal);
+
+   AtClusterizeTask *clusterizer = new AtClusterizeTask();
+   clusterizer->SetPersistence(kFALSE);
+
+   AtPulseTask *pulse = new AtPulseTask(std::make_shared<AtPulse>(mapping));
+   pulse->SetPersistence(kTRUE);
+
+   auto psa = std::make_unique<AtPSAMax>();
+   psa->SetThreshold(0);
+
+   // Create PSA task
+   AtPSAtask *psaTask = new AtPSAtask(std::move(psa));
+   psaTask->SetPersistence(kTRUE);
+
+   AtPRAtask *praTask = new AtPRAtask();
+   praTask->SetPersistence(kTRUE);
+
+   fRun->AddTask(clusterizer);
+   fRun->AddTask(pulse);
+   fRun->AddTask(psaTask);
+   fRun->AddTask(praTask);
+
+   //  __ Init and run ___________________________________
+   fRun->Init();
+
+   timer.Start();
+   fRun->Run(0, 5000);
+   timer.Stop();
+
+   std::cout << std::endl << std::endl;
+   std::cout << "Macro finished succesfully." << std::endl << std::endl;
+   // -----   Finish   -------------------------------------------------------
+
+   Double_t rtime = timer.RealTime();
+   Double_t ctime = timer.CpuTime();
+   cout << endl;
+   cout << "Real time " << rtime << " s, CPU time " << ctime << " s" << endl;
+   cout << endl;
+   // ------------------------------------------------------------------------
+}
+
+bool reduceFunc(AtRawEvent *evt)
+{
+   return (evt->GetNumPads() > 0) && evt->IsGood();
+}
