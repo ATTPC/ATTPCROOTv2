68ed7f111e026e55ff67edee0272549530f2f28e
anthoak13 <anthonya@frib.msu.edu>
2023-09-23 15:46:16 -0400

    Fix implementation of OpenKF

--- AtReconstruction/AtFitter/AtOpenKFTest.h
+++ AtReconstruction/AtFitter/AtOpenKFTest.h
@@ -1,9 +1,13 @@
 #ifndef __ATOPENKFTEST_H__
 #define __ATOPENKFTEST_H__
 
-#include <kalman_filter.h>
+#include <types.h>
+
+#include <unscented_kalman_filter.h>
 
 class AtOpenKFTest {
+
+   kf::Vector<2> x;
 };
 
 #endif //__ATOPENKFTEST_H__
