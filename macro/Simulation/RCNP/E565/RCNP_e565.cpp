19fecab871c870ba0474ca44994e1ff53920ac00
RealAurio <oirua@proton.me>
2025-10-24 15:49:11 +0200

    E565: Changed pressure to 600Torr and beam energy to 21MeV/u. Also cleaned up the simulation macro a bit and addedthe eventDisplay macro.

--- macro/Simulation/RCNP/E565/RCNP_e565.cpp
+++ macro/Simulation/RCNP/E565/RCNP_e565.cpp
@@ -1,176 +1,142 @@
 // 12Be (d,p) 13Be
-// Beam energy 18 MeV/u
+// Beam energy 21 MeV/u
 
 void RCNP_e565(Int_t nEvents = 10000, TString mcEngine = "TGeant4")
 {
+   Double_t ThetaMinCMS = 0.0;
+   Double_t ThetaMaxCMS = 50.0;
 
-  TString dir = getenv("VMCWORKDIR");
+   TString dir = getenv("VMCWORKDIR");
 
-  // Output file name
-  TString outFile ="rcnp_e565_180deg_600torr.root";
+   // Output file name
+   TString outFile = TString::Format("./attpcsim_13Be_p_%.1f_%.1f_600Torr.root", ThetaMinCMS, ThetaMaxCMS);
 
-  // Parameter file name
-  TString parFile="./data/attpcpar_e565_180deg_600torr.root";
+   // Parameter file name
+   TString parFile = TString::Format("./data/attpcpar_13Be_p_%.1f_%.1f_600Torr.root", ThetaMinCMS, ThetaMaxCMS);
 
-  // -----   Timer   --------------------------------------------------------
-  TStopwatch timer;
-  timer.Start();
-  // ------------------------------------------------------------------------
+   // -----   Timer   --------------------------------------------------------
+   TStopwatch timer;
+   timer.Start();
+   // ------------------------------------------------------------------------
 
-  //ATVertexPropagator* vertex_prop = new ATVertexPropagator();
+   Bool_t fApolloDigitizer = true; // Apply hit digitizer task for Apollo
 
-  Bool_t fApolloDigitizer = true; // Apply hit digitizer task for Apollo
-
-  // -----   Create simulation run   ----------------------------------------
-  FairRunSim* run = new FairRunSim();
-  run->SetName(mcEngine);              // Transport engine
-  run->SetOutputFile(outFile);          // Output file
-  FairRuntimeDb* rtdb = run->GetRuntimeDb();
-  // ------------------------------------------------------------------------
-
-
-  // -----   Create media   -------------------------------------------------
-  run->SetMaterials("media.geo");       // Materials
-  // ------------------------------------------------------------------------
-
-  // -----   Create geometry   ----------------------------------------------
-
-  FairModule* cave= new AtCave("CAVE");
-  cave->SetGeometryFileName("cave.geo");
-  run->AddModule(cave);
-
-  FairDetector* ATTPC = new AtTpc("ATTPC", kTRUE);
-  ATTPC->SetGeometryFileName("RCNP_ATTPC_600torr.root");
-  //HELIOS->SetModifyGeometry(kTRUE);
-  run->AddModule(ATTPC);
-  
-  FairDetector* RCNP_Si = new AtSiArray("Si", kTRUE);
-  RCNP_Si->SetGeometryFileName("RCNP_Si.root");
-  //HELIOS->SetModifyGeometry(kTRUE);
-  run->AddModule(RCNP_Si);
-
-  FairDetector* RCNP_GAGG = new AtApollo("GAGG", kTRUE);
-  RCNP_GAGG->SetGeometryFileName("RCNP_GAGG.root");
-  //ATTPC->SetModifyGeometry(kTRUE);
-  run->AddModule(RCNP_GAGG);
-
- // ------------------------------------------------------------------------
+   // -----   Create simulation run   ----------------------------------------
+   FairRunSim* run = new FairRunSim();
+   run->SetName(mcEngine);              // Transport engine
+   run->SetOutputFile(outFile);          // Output file
+   FairRuntimeDb* rtdb = run->GetRuntimeDb();
+   // ------------------------------------------------------------------------
 
 
-    // -----   Magnetic field   -------------------------------------------
-    // Constant Field
-    //AtConstField  *fMagField = new AtConstField();
-    //fMagField->SetField(0., 0. ,0. ); // values are in kG
-    //fMagField->SetFieldRegion(-100, 100,-100, 100, -250,250); // values are in cm
-    //fMagField->Print();
-                          //  (xmin,xmax,ymin,ymax,zmin,zmax)
-    //run->SetField(fMagField);
-    // --------------------------------------------------------------------
+   // -----   Create media   -------------------------------------------------
+   run->SetMaterials("media.geo");       // Materials
+   // ------------------------------------------------------------------------
+
+   // -----   Create geometry   ----------------------------------------------
+   FairModule* cave= new AtCave("CAVE");
+   cave->SetGeometryFileName("cave.geo");
+   run->AddModule(cave);
+
+   FairDetector* ATTPC = new AtTpc("ATTPC", kTRUE);
+   ATTPC->SetGeometryFileName("RCNP_ATTPC_600torr.root");
+   run->AddModule(ATTPC);
+
+   FairDetector* RCNP_Si = new AtSiArray("Si", kTRUE);
+   RCNP_Si->SetGeometryFileName("RCNP_Si.root");
+   run->AddModule(RCNP_Si);
+
+   FairDetector* RCNP_GAGG = new AtApollo("GAGG", kTRUE);
+   RCNP_GAGG->SetGeometryFileName("RCNP_GAGG.root");
+   run->AddModule(RCNP_GAGG);
+
+   // ------------------------------------------------------------------------
+
+   // -----   Create PrimaryGenerator   --------------------------------------
+   FairPrimaryGenerator* primGen = new FairPrimaryGenerator();
+
+   // Beam Information
+   Int_t z = 4;  // Atomic number
+	Int_t a = 12; // Mass number
+	Int_t q = 0;   // Charge State
+	Int_t m = 1;   // Multiplicity  NOTE: Due the limitation of the TGenPhaseSpace accepting only pointers/arrays the maximum multiplicity has been set to 10 particles.
+	Double_t px = 0.000/a;  // X-Momentum / per nucleon!!!!!!
+	Double_t py = 0.000/a;  // Y-Momentum / per nucleon!!!!!!
+	Double_t pz = 2.392/a;  // Z-Momentum / per nucleon!!!!!!
+  	Double_t BExcEner = 0.0;
+   Double_t Bmass = 12.02473; //
+   Double_t NomEnergy = 74.52; //Used to force the beam to stop within a certain energy range.
+
+   AtTPCIonGenerator* ionGen = new AtTPCIonGenerator("Ion",z,a,q,m,px,py,pz,BExcEner,Bmass,NomEnergy);
+	ionGen->SetSpotRadius(0,-100,0);
+	primGen->AddGenerator(ionGen); // add the ion generator
+
+   //primGen->SetBeam(1,1,0,0); //These parameters change the position of the vertex of every track added to the Primary Generator
+   //primGen->SetTarget(30,0);
+
+   // Variables for 2-Body kinematics reaction
+   std::vector<Int_t> Zp; // Zp
+	std::vector<Int_t> Ap; // Ap
+   std::vector<Int_t> Qp;//Electric charge
+   Int_t mult;  //Number of particles
+ 	std::vector<Double_t> Pxp; //Px momentum X
+	std::vector<Double_t> Pyp; //Py momentum Y
+	std::vector<Double_t> Pzp; //Pz momentum Z
+   std::vector<Double_t> Mass; // Masses
+	std::vector<Double_t> ExE; // Excitation energy
+ 	Double_t ResEner; // Energy of the beam (Useless for the moment)
+
+   // Note: Momentum will be calculated from the phase Space according to the residual energy of the beam
+
+   mult = 4; //Number of Nuclei involved in the reaction (Should be always 4) THIS DEFINITION IS MANDATORY (and the number of particles must be the same)
+   ResEner = 0.0; // For fixed target mode (Si Array) in MeV
+
+   // ---- Beam ----
+   Zp.push_back(z); //
+   Ap.push_back(a); //
+   Qp.push_back(q);
+   Pxp.push_back(px);
+   Pyp.push_back(py);
+   Pzp.push_back(pz);
+   Mass.push_back(Bmass);
+   ExE.push_back(BExcEner);
+
+   // ---- Target ----
+   Zp.push_back(1); //
+	Ap.push_back(2); //
+	Qp.push_back(0); //
+	Pxp.push_back(0.0);
+   Pyp.push_back(0.0);
+	Pzp.push_back(0.0);
+   Mass.push_back(2.0135532);
+	ExE.push_back(0.0);//In MeV
+
+   //--- Scattered -----
+   Zp.push_back(4); //
+   Ap.push_back(13); //
+   Qp.push_back(0);
+   Pxp.push_back(0.0);
+   Pyp.push_back(0.0);
+   Pzp.push_back(0.0);
+   Mass.push_back(13.03394);
+   ExE.push_back(0.0);
 
 
+   // ---- Recoil -----
+	Zp.push_back(1); // p
+	Ap.push_back(1); //
+	Qp.push_back(0); //
+	Pxp.push_back(0.0);
+   Pyp.push_back(0.0);
+	Pzp.push_back(0.0);
+   Mass.push_back(1.00783);
+	ExE.push_back(0.0);//In MeV
 
-  // -----   Create PrimaryGenerator   --------------------------------------
-  FairPrimaryGenerator* primGen = new FairPrimaryGenerator();
-
-
-
-
-                  // Beam Information
-                Int_t z = 4;  // Atomic number
-	        Int_t a = 12; // Mass number
-	        Int_t q = 0;   // Charge State
-	        Int_t m = 1;   // Multiplicity  NOTE: Due the limitation of the TGenPhaseSpace accepting only pointers/arrays the maximum multiplicity has been set to 10 particles.
-	        Double_t px = 0.000/a;  // X-Momentum / per nucleon!!!!!!
-	        Double_t py = 0.000/a;  // Y-Momentum / per nucleon!!!!!!
-	        Double_t pz = 2.21/a;  // Z-Momentum / per nucleon!!!!!!
-  	        Double_t BExcEner = 0.0;
-                Double_t Bmass = 12.02473; //
-                Double_t NomEnergy = 90.7; //Used to force the beam to stop within a certain energy range.
-
-
-
-	          AtTPCIonGenerator* ionGen = new AtTPCIonGenerator("Ion",z,a,q,m,px,py,pz,BExcEner,Bmass,NomEnergy);
-	        ionGen->SetSpotRadius(0,-100,0);
-	          // add the ion generator
-
-	          primGen->AddGenerator(ionGen);
-
-  		  //primGen->SetBeam(1,1,0,0); //These parameters change the position of the vertex of every track added to the Primary Generator
-		  // primGen->SetTarget(30,0);
-
-
-
-
-		 // Variables for 2-Body kinematics reaction
-                  std::vector<Int_t> Zp; // Zp
-		  std::vector<Int_t> Ap; // Ap
-                  std::vector<Int_t> Qp;//Electric charge
-                  Int_t mult;  //Number of particles
- 		  std::vector<Double_t> Pxp; //Px momentum X
-		  std::vector<Double_t> Pyp; //Py momentum Y
-		  std::vector<Double_t> Pzp; //Pz momentum Z
-                  std::vector<Double_t> Mass; // Masses
-		  std::vector<Double_t> ExE; // Excitation energy
- 		  Double_t ResEner; // Energy of the beam (Useless for the moment)
-
-
-		  // Note: Momentum will be calculated from the phase Space according to the residual energy of the beam
-
-
-	          mult = 4; //Number of Nuclei involved in the reaction (Should be always 4) THIS DEFINITION IS MANDATORY (and the number of particles must be the same)
-                  ResEner = 0.0; // For fixed target mode (Si Array) in MeV
-
-                  // ---- Beam ----
-                  Zp.push_back(z); //
-            	  Ap.push_back(a); //
-            	  Qp.push_back(q);
-                  Pxp.push_back(px);
-            	  Pyp.push_back(py);
-            	  Pzp.push_back(pz);
-            	  Mass.push_back(Bmass);
-            	  ExE.push_back(BExcEner);
-
-                  // ---- Target ----
-                 Zp.push_back(1); //
-		 Ap.push_back(2); //
-		 Qp.push_back(0); //
-		 Pxp.push_back(0.0);
-                 Pyp.push_back(0.0);
-		 Pzp.push_back(0.0);
-                 Mass.push_back(2.0135532);
-		 ExE.push_back(0.0);//In MeV
-
-                  //--- Scattered -----
-                Zp.push_back(4); //
-                Ap.push_back(13); //
-                Qp.push_back(0);
-          	Pxp.push_back(0.0);
-          	Pyp.push_back(0.0);
-          	Pzp.push_back(0.0);
-          	Mass.push_back(13.03394);
-          	ExE.push_back(0.0);
-
-
-                 // ---- Recoil -----
-		 Zp.push_back(1); // p
-		 Ap.push_back(1); //
-		 Qp.push_back(0); //
-		 Pxp.push_back(0.0);
-                 Pyp.push_back(0.0);
-		 Pzp.push_back(0.0);
-                 Mass.push_back(1.00783);
-		 ExE.push_back(0.0);//In MeV
-
-
-                 Double_t ThetaMinCMS = 170.0;
-                 Double_t ThetaMaxCMS = 180.0;
-
-
-        AtTPC2Body* TwoBody = new AtTPC2Body("TwoBody",&Zp,&Ap,&Qp,mult,&Pxp,&Pyp,&Pzp,&Mass,&ExE,ResEner, ThetaMinCMS,ThetaMaxCMS);
-        //TwoBody->SetFixedTargetPosition(0.0,0.0,0.0);
-        //TwoBody->SetFixedBeamMomentum(0.0,0.0,pz*a);
-        //TwoBody->SetSequentialDecay(kTRUE);
-        primGen->AddGenerator(TwoBody);
+   AtTPC2Body* TwoBody = new AtTPC2Body("TwoBody",&Zp,&Ap,&Qp,mult,&Pxp,&Pyp,&Pzp,&Mass,&ExE,ResEner, ThetaMinCMS,ThetaMaxCMS);
+   //TwoBody->SetFixedTargetPosition(0.0,0.0,0.0);
+   //TwoBody->SetFixedBeamMomentum(0.0,0.0,pz*a);
+   //TwoBody->SetSequentialDecay(kTRUE);
+   primGen->AddGenerator(TwoBody);
 
 
         // -----   Create GammaDummyGenerator
---
+++ macro/Simulation/RCNP/E565/eventDisplay.C
@@ -0,0 +1,34 @@
+void eventDisplay()
+{
+   //-----User Settings:-----------------------------------------------
+   TString  InputDataFile     ="attpcsim_13Be_p_0.0_50.0_600Torr.root";
+   TString  ParFile       ="./data/attpcpar_13Be_p_0.0_50.0_600Torr.root";
+   TString  OutputDataFile	 ="rcnpe565_600torr.root";
+
+   // -----   Reconstruction run   -------------------------------------------
+   FairRunAna *fRun = new FairRunAna();
+   FairRootFileSink *sink = new FairRootFileSink(OutputDataFile);
+   FairFileSource *source = new FairFileSource(InputDataFile);
+   fRun->SetSource(source);
+   fRun->SetSink(sink);
+   // fRun->SetGeomFile(GeoDataPath);
+
+   FairRuntimeDb *rtdb = fRun->GetRuntimeDb();
+   FairParRootFileIo *parInput1 = new FairParRootFileIo();
+   parInput1->open(ParFile.Data());
+   rtdb->setFirstInput(parInput1);
+
+   FairEventManager *fMan = new FairEventManager();
+
+   //----------------------Traks and points -------------------------------------
+   // FairMCTracks *Track = new FairMCTracks("Monte-Carlo Tracks");
+   FairMCPointDraw *AtTpcPoints = new FairMCPointDraw("AtTpcPoint", kBlue, kFullSquare);
+   FairMCPointDraw *AtSiArrayPoints = new FairMCPointDraw("AtSiArrayPoint", kBlue, kFullSquare);
+
+
+   // fMan->AddTask(Track);
+   fMan->AddTask(AtTpcPoints);
+   fMan->AddTask(AtSiArrayPoints);
+
+   fMan->Init();
+}
