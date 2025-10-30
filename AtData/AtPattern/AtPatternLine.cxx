ee81dea58f218c76d4cb1232f6add69fb43ae9bb
RealAurio <oirua@proton.me>
2025-10-30 18:35:55 +0100

    Changed how the geometric angles are calculated for the AtTracks in the AtSampleConsensus. The angles from the AtPatternLine are not reliable since the points obtained by the RANSAC may not be on the same side of the AtTrack as the AtHits, so sometimes the angle was flipped around.

--- AtData/AtPattern/AtPatternLine.cxx
+++ AtData/AtPattern/AtPatternLine.cxx
@@ -215,13 +215,3 @@ Double_t AtPatternLine::DistanceAlongPattern(XYZPoint point1, XYZPoint point2) c
 {
    return DefaultDistanceAlongPattern(point1, point2);
 }
-
-Double_t AtPatternLine::CalculateTheta() const
-{
-   return TMath::Pi() - GetDirection().Theta();
-}
-
-Double_t AtPatternLine::CalculatePhi() const
-{
-   return GetDirection().Phi();
-}
