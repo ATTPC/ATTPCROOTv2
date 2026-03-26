0c3e7fc7868068f52f3bd0e5465fcb2788460658
RealAurio <oirua@proton.me>
2026-03-26 16:54:51 +0100

    Fixed how the baseline is substracted for the case of negative traces. Also roughly substracted the baseline from the integral of the entire trace.

--- AtReconstruction/AtPulseAnalyzer/AtPSASi.cxx
+++ AtReconstruction/AtPulseAnalyzer/AtPSASi.cxx
@@ -109,17 +109,17 @@ AtPSASi::HitVector AtPSASi::AnalyzeGenTrace(AtGenericTrace *genTrace)
    Double_t charge{-999};
    if (fPositivePolarity) {
       maxAdcIt = std::max_element(floatADC.begin() + 20, floatADC.end() - 12);
-      charge = *maxAdcIt;
+      charge = *maxAdcIt - baseline;
    } else {
       maxAdcIt = std::min_element(floatADC.begin() + 20, floatADC.end() - 12);
-      charge = -(*maxAdcIt);
+      charge = -(*maxAdcIt - baseline);
    }
    Int_t maxAdcIdx = std::distance(floatADC.begin(), maxAdcIt);
    // std::cout << " Max ADC = " << *maxAdcIt << std::endl;
 
    // std::cout << " Diff ADC = " << *maxAdcIt - baseline << std::endl;
 
-   if (!shouldSaveHit(charge, baseline + fThreshold, maxAdcIdx)) {
+   if (!shouldSaveHit(charge, fThreshold, maxAdcIdx)) {
       LOG(debug) << "GAGG trace did not pass threshold.";
       return {};
    }
@@ -127,9 +127,9 @@ AtPSASi::HitVector AtPSASi::AnalyzeGenTrace(AtGenericTrace *genTrace)
    // Calculation of the mean value of the peak time by interpolating the pulse
    Double_t timemax = 0.5 * (floatADC[maxAdcIdx - 1] - floatADC[maxAdcIdx + 1]) /
                       (floatADC[maxAdcIdx - 1] + floatADC[maxAdcIdx + 1] - 2 * floatADC[maxAdcIdx]);
-   Double_t QHitTot = std::abs(std::accumulate(floatADC.begin(), floatADC.end(), 0));
+   Double_t QHitTot = std::abs(std::accumulate(floatADC.begin(), floatADC.end(), 0)) - baseline * floatADC.size();
 
-   auto hit = std::make_unique<AtHit>(0, pos, charge - baseline);
+   auto hit = std::make_unique<AtHit>(0, pos, charge);
 
    hit->SetTimeStamp(maxAdcIdx);
    hit->SetTimeStampCorrInter(timemax);
